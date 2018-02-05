f7bc6b1454ca4c2f9e21f16b2036a1985ada0dac
Joachim Moeyens <moeyensj@gmail.com>
2018-02-05 12:41:08 -0800

    Implemented findObsInCell (with unittest)

---
+++ rascals/data_processing.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+__all__ = ["findObsInCell"]
+
+def findObsInCell(obsIds, coords, coord_center, radius):
+    """
+    Find the observation IDs in a circular / spherical region 
+    about a central point.
+    
+    Parameters
+    ----------
+    obsIds : `~np.ndarray` [N, 1]
+        Array of observation IDs corresponding to the coords.
+    coords : `~np.ndarray` [N, d]
+        Array of coordinates of N rows for each observation
+        and d dimensions. 
+    coord_center : `~np.ndarray` [1, d]
+        Array containing coordinates in d dimensions about which
+        to search. 
+    radius : float
+        Search radius.
+        
+    Returns
+    -------
+    `~np.ndarray`
+        Array of observation IDs that fall within the search radius.
+    """
+    distances = np.sqrt(np.sum((coords - coord_center)**2, axis=1))
+    return obsIds[np.where(distances <= radius)[0]]
