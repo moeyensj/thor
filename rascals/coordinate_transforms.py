d2ed2a2e5ed4d5bf115c372be1675cde1fe76865
Joachim Moeyens <moeyensj@gmail.com>
2018-02-20 11:13:42 -0800

    Removed calcNae (to be in separate module); added all necessary coordinate transforms with unittests

--- rascals/coordinate_transforms.py
+++ rascals/coordinate_transforms.py
@@ -1,43 +1,102 @@
 import numpy as np
 
-__all__ = ["_angularToCartesian",
+OBLIQUITY = np.radians(23.43928)
+TRANSFORM_EQ2EC = np.matrix([[1, 0 , 0],
+                             [0,  np.cos(OBLIQUITY), np.sin(OBLIQUITY)],
+                             [0, -np.sin(OBLIQUITY), np.cos(OBLIQUITY)]])
+TRANSFORM_EC2EQ = np.matrix([[1, 0 , 0],
+                             [0, np.cos(OBLIQUITY), -np.sin(OBLIQUITY)],
+                             [0, np.sin(OBLIQUITY),  np.cos(OBLIQUITY)]])
+
+
+__all__ = ["equatorialToEclipticCartesian",
+           "eclipticToEquatorialCartesian",
+           "_angularToCartesian",
            "eclipticAngularToCartesian",
            "equatorialAngularToCartesian",
-           "calcNae"]
+           "_cartesianToAngular",
+           "eclipticCartesianToAngular",
+           "equatorialCartesianToAngular",
+           "eclipticToEquatorialAngular",
+           "equatorialToEclipticAngular"]
 
-def _angularToCartesian(coords, dist):
+# Cartesian to Cartesian
+
+def equatorialToEclipticCartesian(coords_eq_cart):
     """
-    Converts angular coordinates to cartesian given
-    an angle or angles of longitude (alpha), an angle or angles of latitude (beta),
-    and distance. 
-    
-    Input array should have shape (N, 2):
-    np.array([[lon1, lat1],
-              [lon2, lat2],
-              [lon3, lat3],
-                  .....
-              [lonN, latN]])
+    Transform equatorial cartesian coordinates to ecliptic cartesian
+    coordinates. 
     
     Parameters
     ----------
-    coords : `~np.ndarray` (N, 2)
-        Longitude in radians, latitude in radians.
-    dist : float or `~np.ndarray` (N)
-        Distance in arbitrary units.
+    coords_eq_cart : `~np.ndarray` (N, 3)
+        Cartesian equatorial x, y, z coordinates.
         
     Returns
     -------
-    coords_cart : `~np.ndarray` (N, 3)
+    coords_ec_cart : `~np.ndarray` (N, 3)
+        Cartesian ecliptic x, y, z coordinates.
+        
+    See Also
+    --------
+    eclipticToEquatorialCartesian : Transform ecliptic cartesian coordinates 
+        to equatorial cartesian coordinates. 
+    """
+    return np.array(TRANSFORM_EQ2EC @ coords_eq_cart.T).T
+    
+def eclipticToEquatorialCartesian(coords_ec_cart):
+    """
+    Transform ecliptic cartesian coordinates to equatorial cartesian
+    coordinates. 
+     
+    
+    Parameters
+    ----------
+    coords_ec_cart : `~np.ndarray` (N, 3)
+        Cartesian ecliptic x, y, z coordinates.
+     
+    Returns
+    -------
+    coords_eq_cart : `~np.ndarray` (N, 3)
+        Cartesian equatorial x, y, z coordinates.
+        
+    See Also
+    --------
+    equatorialToEclipticCartesian : Transform ecliptic cartesian coordinates 
+        to equatorial cartesian coordinates. 
+    """
+    return np.array(TRANSFORM_EC2EQ @ coords_ec_cart.T).T
+
+# Angular to Cartesian
+
+def _angularToCartesian(phi, theta, r):
+    """
+    Converts angular coordinates to cartesian given
+    an angle or angles of longitude (phi), an angle or angles of latitude (theta),
+    and distance (r). 
+    
+    Parameters
+    ----------
+    phi : `~np.ndarray` (N)
+        Longitude in radians.
+    theta : `~np.ndarray` (N)
+        Latitude in radians. 
+    r : float or `~np.ndarray` (N)
+        Radial distance in arbitrary units.
+        
+    Returns
+    -------
+    x, y, z: `~np.ndarray` (N, 3)
         Cartesian x, y, z coordinates.
     """
-    x = dist * np.cos(coords[:,1]) * np.cos(coords[:,0])
-    y = dist * np.cos(coords[:,1]) * np.sin(coords[:,0]) 
-    z = dist * np.sin(coords[:,1]) 
+    x = r * np.cos(theta) * np.cos(phi)
+    y = r * np.cos(theta) * np.sin(phi) 
+    z = r * np.sin(theta) 
     return np.array([x, y, z]).T
 
-def eclipticAngularToCartesian(coords_ec_ang, dist):
+def eclipticAngularToCartesian(coords_ec_ang, delta=1):
     """
-    Convert angular ecliptic coordinates to 
+    Convert ecliptic coordinates to 
     cartesian coordinates.
     
     Input coordinate array should have shape (N, 2):
@@ -51,23 +110,23 @@ def eclipticAngularToCartesian(coords_ec_ang, dist):
     ----------
     coords_ec_ang : `~np.ndarray` (N, 2)
         Ecliptic longitude and latitude in radians.
-    dist : float or `~np.ndarray` (N)
+    dist : float or `~np.ndarray` (N), optional
         Distance in arbitrary units.
+        [Default = 1]
         
     Returns
     -------
     coords_ec_cart : `~np.ndarray` (N, 3)
-        Cartesian ecliptic x, y, z coordinates
-        in same units as dist.
+        Cartesian x, y, z coordinates.
         
     See Also
     --------
     equatorialAngularToCartesian : Convert angular equatorial
     coordinates to equatorial cartesian.
     """
-    return _angularToCartesian(coords_ec_ang, dist)
+    return _angularToCartesian(coords_ec_ang[:,0], coords_ec_ang[:,1], delta)
 
-def equatorialAngularToCartesian(coords_eq_ang, dist):
+def equatorialAngularToCartesian(coords_eq_ang, r=1):
     """
     Convert angular equatorial coordinates to 
     cartesian coordinates.
@@ -82,27 +141,93 @@ def equatorialAngularToCartesian(coords_eq_ang, dist):
     Parameters
     ----------
     coords_eq_ang : `~np.ndarray` (N, 2)
-        Right Ascension and Declination in radians.
-    dist : float or `~np.ndarray` (N)
+        Right Ascenion and declination in radians.
+    r : float or `~np.ndarray` (N), optional
         Distance in arbitrary units.
+        [Default = 1]
         
     Returns
     -------
     coords_eq_cart : `~np.ndarray` (N, 3)
-        Cartesian equatorial x, y, z coordinates
-        in same units as dist.
+        Cartesian x, y, z coordinates.
         
     See Also
     --------
     eclipticAngularToCartesian : Convert angular ecliptic
     coordinates to ecliptic cartesian.
     """
-    return _angularToCartesian(coords_eq_ang, dist)
+    return _angularToCartesian(coords_eq_ang[:,0], coords_eq_ang[:,1], r)
 
-def calcNae(coords_ec_ang):
+# Cartesian to Angular
+
+def _cartesianToAngular(x, y, z):
     """
-    Convert angular ecliptic coordinates to 
-    to a cartesian unit vector. 
+    Convert cartesian coordinates to angular coordinates.
+    
+    Parameters
+    ----------
+    x : `~np.ndarray` (N)
+        Cartesian x coordinate.
+    y : `~np.ndarray` (N)
+        Cartesian y coordinate.
+    z : `~np.ndarray` (N)
+        Cartesian z coordinate.
+    
+    Returns
+    -------
+    phi, theta, r : `~np.ndarray` (N, 3)
+        Longitude and latitude in radians and 
+        radial distance in arbitrary units.
+    """
+    r = np.sqrt(x**2 + y**2 + z**2)
+    phi = np.arctan2(y, x)
+    theta = np.arcsin(z / r)
+    phi = np.where(phi < 0, phi + 2*np.pi, phi)
+    return np.array([phi, theta, r]).T
+
+def eclipticCartesianToAngular(coords_ec_cart):
+    """
+    Convert ecliptic cartesian coordinates to 
+    angular coordinates.
+    
+    Parameters
+    ----------
+    coords_ec_cart : `~np.ndarray` (N, 3)
+        Cartesian ecliptic x, y, z coordinates.
+    
+    Returns
+    -------
+    coords_eq_ang : `~np.ndarray` (N, 3)
+        Ecliptic longitude and latitude in radians
+        and radial distance in arbitrary units.
+    """
+    return _cartesianToAngular(*coords_ec_cart.T)
+
+def equatorialCartesianToAngular(coords_eq_cart):
+    """
+    Convert equatorial cartesian coordinates to 
+    angular coordinates.
+    
+    
+    Parameters
+    ----------
+    coords_eq_cart : `~np.ndarray` (N, 3)
+        Cartesian equatorial x, y, z coordinates.
+    
+    Returns
+    -------
+    coords_eq_ang : `~np.ndarray` (N, 3)
+        Equatorial longitude and latitude in radians
+        and radial distance in arbitrary units.
+    """
+    return _cartesianToAngular(*coords_eq_cart.T)
+
+# Angular to Angular
+
+def equatorialToEclipticAngular(coords_eq_ang, r=1):
+    """
+    Transform angular equatorial coordinates to 
+    angular ecliptic coordinates.
     
     Input coordinate array should have shape (N, 2):
     np.array([[lon1, lat1],
@@ -113,13 +238,48 @@ def calcNae(coords_ec_ang):
     
     Parameters
     ----------
+    coords_eq_ang : `~np.ndarray` (N, 2)
+        Right Ascension and Declination in radians.
+    r : float or `~np.ndarray` (N), optional
+        Distance from origin in arbitrary units.
+        [Default = 1]
+            
+    Returns
+    -------
+    coords_ec_ang : `~np.ndarray` (N, 3)
+        Ecliptic longitude and latitude in radians
+        and radial distance in arbitrary units.
+    """
+    coords_eq_cart = equatorialAngularToCartesian(coords_eq_ang, r=r)
+    coords_ec_cart = equatorialToEclipticCartesian(coords_eq_cart)
+    return eclipticCartesianToAngular(coords_ec_cart)
+
+def eclipticToEquatorialAngular(coords_ec_ang, delta=1):
+    """
+    Transform angular ecliptic coordinates to 
+    angular equatorial coordinates.
+    
+    Input coordinate array should have shape (N, 2):
+    np.array([[ra1, dec1],
+              [ra2, dec2],
+              [ra3, dec3],
+                  .....
+              [raN, decN]])
+    
+    Parameters
+    ----------
     coords_ec_ang : `~np.ndarray` (N, 2)
         Ecliptic longitude and latitude in radians.
+    delta : float or `~np.ndarray` (N)
+        Distance from origin in arbitrary units.
+        [Default = 1]
         
     Returns
     -------
-    coords_eq_cart : `~np.ndarray` (N, 3)
-        Cartesian unit vector in direction of provided
-        angular coordinates.
+    coords_eq_ang : `~np.ndarray` (N, 3)
+        Right Ascension and Declination in radians
+        and radial distance in arbitrary units.
     """
-    return _angularToCartesian(coords_ec_ang, 1)
+    coords_ec_cart = eclipticAngularToCartesian(coords_ec_ang, delta=delta)
+    coords_eq_cart = eclipticToEquatorialCartesian(coords_ec_cart)
+    return equatorialCartesianToAngular(coords_eq_cart)
