3cab1eeca288ea9141648fbf2812fb252390b67e
Joachim Moeyens <moeyensj@gmail.com>
2018-02-21 16:20:48 -0800

    Corrected package name in setup.py script

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-   name="RaSCaLS",
-   version="0.2.0.dev0",
+   name="rascals",
+   version="0.2.1.dev0",
    description="Range, Shift, Cluster and Link Scheme",
    license="BSD 3-Clause License",
    author="Joachim Moeyens, Mario Juric",
    author_email="moeyensj@uw.edu",
    url="https://github.com/moeyensj/RaSCaLS",
-   packages=["RaSCaLS"],
-   package_dir={"RaSCaLS": "rascals"},
-   package_data={"RaSCaLS": ["data/*.orb",
+   packages=["rascals"],
+   package_dir={"rascals": "rascals"},
+   package_data={"rascals": ["data/*.orb",
                              "tests/data/*"]},
    setup_requires=["pytest-runner"],
    tests_require=["pytest"],
