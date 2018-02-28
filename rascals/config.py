ebf8bbeaf3b06e8589d7ca5808b7cb529047e88b
Joachim Moeyens <moeyensj@gmail.com>
2018-02-28 08:20:28 -0800

    Simplified phase folding notebook; properly propagate using test particle; bug with certain propagations needs fixing

--- rascals/config.py
+++ rascals/config.py
@@ -1,32 +1,27 @@
 __all__ = ["Config"]
 
 class Config:
-    objectColumns = {"name": "designation"}
-    observationColumns = {"obsId": "obsId",
-                          "mjd": "mjd_utc",
+    
+    observationColumns = {"obs_id" : "obsId",
+                          "exp_mjd" : "mjd_utc",
                           "night": "night",
-                          "obs_x": "HEclObsy_X_au",
-                          "obs_y": "HEclObsy_Y_au",
-                          "obs_z": "HEclObsy_Z_au",
-                          "alt": "VMag",
-                          "alpha": "PhaseAngle_deg"}
-    primaryCoordinateSystem = "equatorialAngularTopo"
-    additionalCoordinateSystems = ["eclipticAngularHelio", "eclipticCartesianHelio"]
-    coordinateColumns = {"equatorialAngularTopo": ["RA_deg",
-                                                   "Dec_deg",
-                                                   "Delta_au"],
-                         "eclipticAngularHelio": ["HLon_deg",
-                                                  "HLat_deg",
-                                                  "r_au"],
-                         "eclipticCartesianHelio": ["HEclObj_X_au",
-                                                    "HEclObj_Y_au",
-                                                    "HEclObj_Z_au"]}
-    velocityColumns = {"equatorialAngularTopo": ["dRA/dt_deg_p_day",
-                                                 "dDec/dt_deg_p_day",
-                                                 "dDelta/dt_au_p_day"],
-                       "eclipticCartesianHelio": ["HEclObj_dX/dt_au_p_day",
-                                                  "HEclObj_dY/dt_au_p_day",
-                                                  "HEclObj_dZ/dt_au_p_day"]}
+                          "RA_deg" : "RA_deg",
+                          "Dec_deg" : "Dec_deg",
+                          "obs_x_au" : "HEclObsy_X_au",
+                          "obs_y_au" : "HEclObsy_Y_au",
+                          "obs_z_au" : "HEclObsy_Z_au",
+                          "mag" : "VMag"}
+    
+    truthColumns = {"name" : "designation",
+                    "Delta_au" : "Delta_au",
+                    "r_au" : "r_au",
+                    "obj_x_au" :"HEclObj_X_au",
+                    "obj_y_au" : "HEclObj_Y_au",
+                    "obj_z_au" : "HEclObj_Z_au",
+                    "obj_dx/dt_au_p_day" : "HEclObj_dX/dt_au_p_day",
+                    "obj_dy/dt_au_p_day" : "HEclObj_dY/dt_au_p_day",
+                    "obj_dz/dt_au_p_day" : "HEclObj_dZ/dt_au_p_day"}
+    
     # MUST BE SET ACCORDINGLY
     ### TODO: have code search for oorb directory
     ###       use a pre-defined configuration file for oorb
