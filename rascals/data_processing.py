1c15991082eda580e1c983dafe0eb0cd83dffe24
Joachim Moeyens <moeyensj@gmail.com>
2018-05-06 20:25:22 -0700

    Moved code from development notebook to package; still need unittests

--- rascals/data_processing.py
+++ rascals/data_processing.py
@@ -1,6 +1,13 @@
 import numpy as np
+import pandas as pd
 
-__all__ = ["findObsInCell"]
+from .config import Config
+
+__all__ = ["findObsInCell",
+           "createQuery",
+           "queryNight",
+           "queryCell",
+           "getObservations"]
 
 def findObsInCell(obsIds, coords, coord_center, radius):
     """
@@ -27,3 +34,100 @@ def findObsInCell(obsIds, coords, coord_center, radius):
     """
     distances = np.sqrt(np.sum((coords - coord_center)**2, axis=1))
     return obsIds[np.where(distances <= radius)[0]]
+
+def createQuery(queryType,
+                observationColumns=Config.observationColumns,
+                truthColumns=Config.truthColumns):
+    if queryType == "observation":
+        columns = list(observationColumns.values())
+    elif queryType == "truth":
+        columns = list(observationColumns.values()) + list(truthColumns.values())
+    else:
+        raise ValueError("queryType should be one of 'observation' or 'truth'")
+    query = '"' + '", "'.join(columns) + '"'
+    return query
+
+def queryNight(con,
+               night,
+               queryType="observation",
+               observationColumns=Config.observationColumns,
+               truthColumns=Config.truthColumns):
+    
+    query = createQuery(queryType,
+                        observationColumns=observationColumns,
+                        truthColumns=truthColumns)
+    night_df = pd.read_sql("""SELECT {} FROM ephemeris
+                              WHERE night = {}""".format(query, night), con)
+    columnMapping = {**observationColumns, **truthColumns}
+    inverseMapping = {value : key for key, value in columnMapping.items()}
+    night_df.rename(columns=inverseMapping, inplace=True)
+    return night_df
+
+def queryCell(con,
+              cell,
+              queryType="observation",
+              chunksize=50000,
+              observationColumns=Config.observationColumns,
+              truthColumns=Config.truthColumns):
+
+    query = createQuery(queryType,
+                        observationColumns=observationColumns,
+                        truthColumns=truthColumns)
+    
+    chunks = []
+    for chunk in pd.read_sql("""SELECT {0} FROM ephemeris
+                                 WHERE (({1} >= {2} AND {1} <= {3})
+                                 AND ({4} >= {5} AND {4} <= {6})
+                                 AND ({7} >= {8} AND {7} <= {9}))""".format(query,
+                                                                            observationColumns["exp_mjd"],
+                                                                            *cell.mjdRange,
+                                                                            observationColumns["RA_deg"],
+                                                                            *cell.xRange,
+                                                                            observationColumns["Dec_deg"],
+                                                                            *cell.yRange), 
+                             con,
+                             chunksize=chunksize):
+        chunks.append(chunk)
+    
+    cell_df = pd.concat(chunks)
+    columnMapping = {**observationColumns, **truthColumns}
+    inverseMapping = {value : key for key, value in columnMapping.items()}
+    cell_df.rename(columns=inverseMapping, inplace=True)                                                                    
+
+    keep = rascals.findObsInCell(cell_df["obs_id"].values,
+                                 cell_df[["RA_deg", "Dec_deg"]].as_matrix(),
+                                 cell.center,
+                                 cell.radius)
+    cell_df = cell_df[cell_df["obs_id"].isin(keep)]
+    return cell_df
+
+def getObservations(raRange,
+                    decRange,
+                    mjdRange,
+                    con,
+                    queryType="truth",
+                    observationColumns=Config.observationColumns,
+                    truthColumns=Config.truthColumns): 
+        
+        query = createQuery(queryType,
+                            observationColumns=observationColumns,
+                            truthColumns=truthColumns)
+        
+        observations = pd.read_sql("""
+            SELECT {0} FROM ephemeris
+            WHERE (({1} >= {2} AND {1} <= {3})
+            AND ({4} >= {5} AND {4} <= {6})
+            AND ({7} >= {8} AND {7} <= {9}))
+        """.format(query,
+                   observationColumns["exp_mjd"],
+                   *mjdRange,
+                   observationColumns["RA_deg"],
+                   *raRange,
+                   observationColumns["Dec_deg"],
+                   *decRange), con)
+
+        columnMapping = {**observationColumns, **truthColumns}
+        inverseMapping = {value : key for key, value in columnMapping.items()}
+        observations.rename(columns=inverseMapping, inplace=True)
+        print("Found {} observations.".format(len(observations)))
+        return observations
