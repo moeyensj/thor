c50b0607bb6bffb40b3c459153da3693567f4671
Joachim Moeyens <moeyensj@gmail.com>
2018-07-20 17:25:48 +0200

    Added findExpTimes to data_processing

--- rascals/data_processing.py
+++ rascals/data_processing.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-__all__ = ["findObsInCell"]
+from .config import Config
 
-def findObsInCell(obsIds, coords, coordsCenter, fieldArea=10, fieldShape="square"):
+__all__ = ["findObsInCell",
+           "findExpTimes"]
+
+def findObsInCell(obsIds, 
+                  coords,
+                  coordsCenter, 
+                  fieldArea=10, 
+                  fieldShape="square"):
     """
     Find the observation IDs in a circular / spherical region 
     about a central point.
@@ -40,3 +47,81 @@ def findObsInCell(obsIds, coords, coordsCenter, fieldArea=10, fieldShape="square
         raise ValueError("fieldType should be one of 'square' or 'circle'")
     return
    
+def findExpTimes(observations, 
+                 r, 
+                 v, 
+                 mjdStart, 
+                 nights, 
+                 vMax=3.0, 
+                 verbose=True,
+                 columnMapping=Config.columnMapping):
+    
+    """
+    Finds the unique exposure times of all detections that fall within
+    a maximum search radius (set by a maximum angular velocity) for a 
+    test particle defined by r and v at mjdStart. 
+    
+    Parameters
+    ----------
+    observations : `~pandas.DataFrame`
+        DataFrame containing observations.
+    r : float
+        Heliocentric distance in AU.
+    v : `~numpy.ndarray` (1, 3)
+        Velocity vector in AU per day (ecliptic).  
+    mjdStart : float
+        Epoch at which ecliptic coordinates and velocity are measured in MJD.
+    nights : `~numpy.ndarray` (N)
+        List of nights at which to calculate exposure times. Should ideally not include
+        the night containing mjdStart. 
+    vMax : float, optional
+        Maximum angular velocity (in RA and Dec) permitted when searching for exposure times
+        in degrees per day. 
+        [Default = 3.0]
+    verbose : bool, optional
+        Print progress statements? 
+        [Default = True]
+    columnMapping : dict, optional
+        Column name mapping of observations to internally used column names. 
+        [Default = `~rascals.Config.columnMapping`] 
+
+    Returns
+    -------
+    mjds : `~numpy.ndarray`
+        Sorted unique exposure times. 
+    """
+    possible_obs_ids = np.empty(0)
+    ri = r
+    vi = v
+    mjdStarti = mjdStart
+    
+    for night in nights: 
+        
+        # Find exposure time in the middle of the night  
+        mjdEnd = np.median(observations[observations["night"] == night]["exp_mjd"].unique())
+        
+        # Propagate particle
+        ephemeris = propagateTestParticle(ri, 
+                                          vi,
+                                          mjdStarti,
+                                          mjdEnd,
+                                          verbose=verbose)
+        
+        # Find all observation IDs within some maximum angular velocity from the location of the particle
+        obs_ids = observations[observations[columnMapping["night"]] == night][columnMapping["obs_id"]].values
+        dRA = observations[observations[columnMapping["night"]] == night][columnMapping["RA_deg"]].values - ephemeris["RA_deg"].values
+        dDec = observations[observations[columnMapping["night"]] == night][columnMapping["Dec_deg"]].values - ephemeris["Dec_deg"].values
+        dec = ephemeris["Dec_deg"].values + dDec/2
+        dt = observations[observations[columnMapping["night"]] == night][columnMapping["exp_mjd"]].values - ephemeris["mjd_utc"].values
+        v = np.abs(np.sqrt((dRA * np.cos(np.radians(dec)))**2  + dDec**2) / dt)
+        index = np.where(v <= vMax)[0]
+        possible_obs_ids = np.concatenate([possible_obs_ids, obs_ids[index]])
+        
+        # Set new r, v and time to particle's current location
+        ri = ephemeris[['HEclObj_X_au', 'HEclObj_Y_au', 'HEclObj_Z_au']].values[0]
+        vi = ephemeris[['HEclObj_dX/dt_au_p_day', 'HEclObj_dY/dt_au_p_day', 'HEclObj_dZ/dt_au_p_day']].values[0]
+        mjdStarti = mjdEnd
+        
+    mjds = observations[observations[columnMapping["obs_id"]].isin(possible_obs_ids)][columnMapping["exp_mjd"]].unique()
+    mjds.sort()
+    return mjds
