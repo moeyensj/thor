65ccdff766e60b07e03c2f093fb3f891d1e32d83
Joachim Moeyens <moeyensj@gmail.com>
2018-09-19 13:11:18 -0700

    Added _setPercentage helper for plotting

--- thor/plotting/helpers.py
+++ thor/plotting/helpers.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
-__all__ = ["_setAxes"]
+__all__ = ["_setAxes",
+           "_setPercentage"]
 
 def _setAxes(ax, coordinateSystem):
     """
@@ -20,3 +21,15 @@ def _setAxes(ax, coordinateSystem):
         raise ValueError("coordinateSystem should be one of: 'equatorialAngular', 'eclipticAngular', 'tangentPlane'")
     ax.set_aspect("equal")
     return
+
+def _setPercentage(limits, percentage):
+    """
+    Helper function which returns the percetange value measured from the lower limit.
+    For example, _setPercentage([2, 4], 1.0) would return 4 whereas _setPercentage([2, 4], 0.50)
+    would return 3.
+
+    This is particularly useful for putting text on a plot. 
+
+    """
+    return (limits[-1] - limits[0]) * percentage + limits[0]
+
