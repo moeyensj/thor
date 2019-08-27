6143962fdb51967a3901c1f0266f0e03ab46ae21
Joachim Moeyens <moeyensj@gmail.com>
2019-08-27 10:14:45 -0700

    Change Ceres to 1719 Jens in conversion test (Ceres fails at epochs not near the current date)

--- thor/orbits/tests/test_kepler.py
+++ thor/orbits/tests/test_kepler.py
@@ -7,14 +7,14 @@ from astroquery.jplhorizons import Horizons
 from .. import convertCartesianToKeplerian
 
 def test_convertCartesianToKeplerian():
-    # Grab orbital elements and state vectors of Ceres and Eros at the defined epoch 
+    # Grab orbital elements and state vectors of 1719 Jens and Eros at the defined epoch 
     start_epoch = Time("1993-02-02T00:00:00.000", format="isot", scale="utc")
     
-    ceres_horizons = Horizons(id="Ceres", epochs=start_epoch.mjd)
-    ceres_elements = ceres_horizons.elements()
-    ceres_elements = np.array(ceres_elements["a", "e", "incl", "Omega", "w", "M"]).view("float64")
-    ceres_vectors  = ceres_horizons.vectors()
-    ceres_vectors  = np.array(ceres_vectors["x", "y", "z", "vx", "vy", "vz"]).view("float64")
+    jens_horizons = Horizons(id="1719", epochs=start_epoch.mjd)
+    jens_elements = jens_horizons.elements()
+    jens_elements = np.array(jens_elements["a", "e", "incl", "Omega", "w", "M"]).view("float64")
+    jens_vectors  = jens_horizons.vectors()
+    jens_vectors  = np.array(jens_vectors["x", "y", "z", "vx", "vy", "vz"]).view("float64")
     
     eros_horizons = Horizons(id="Eros", epochs=start_epoch.mjd)
     eros_elements = eros_horizons.elements()
@@ -23,4 +23,5 @@ def test_convertCartesianToKeplerian():
     eros_vectors = np.array(eros_vectors["x", "y", "z", "vx", "vy", "vz"]).view("float64")
     
     # Convert cartesian state vector to Keplerian elements 
+    np.testing.assert_allclose(convertCartesianToKeplerian(jens_vectors), jens_elements)
     np.testing.assert_allclose(convertCartesianToKeplerian(eros_vectors), eros_elements)
