df7f5af95977869b073a547f60e68cce62055e7c
Joachim Moeyens <moeyensj@gmail.com>
2019-08-27 16:58:48 -0700

    Add Gauss' method for v2 calculation

--- thor/orbits/iod/gauss.py
+++ thor/orbits/iod/gauss.py
@@ -12,6 +12,8 @@ __all__ = [
     "_calcB",
     "_calcLambdas",
     "_calcRhos",
+    "_calcFG",
+    "calcGauss",
 ]
 
 
@@ -45,3 +47,63 @@ def _calcRhos(lambda1, lambda3, q1, q2, q3, rho1hat, rho2hat, rho3hat, V):
     rho2_mag = np.dot(numerator, np.cross(rho1hat, rho3hat)) / V
     rho3_mag = np.dot(numerator, np.cross(rho1hat, rho2hat)) / (lambda3 * V)
     return np.dot(rho1_mag, rho1hat), np.dot(rho2_mag, rho2hat), np.dot(rho3_mag, rho3hat)
+
+def _calcFG(r2_mag, t32, t21):
+    # Calculate the Lagrange coefficients (Gauss f and g series)
+    f1 = 1 - (1 / 2) * MU / r2_mag**3 * (-t21)**2
+    f3 = 1 - (1 / 2) * MU / r2_mag**3 * t32**2
+    g1 = -t21 - (1 / 6) * MU / r2_mag**3 * (-t21)**2
+    g3 = t32 - (1 / 6) * MU / r2_mag**3 * t32**2
+    return f1, g1, f3, g3
+
+def calcGauss(r1, r2, r3, t1, t2, t3):
+    """
+    Calculates the velocity vector at the second position of an object Gauss'
+    original method.
+    
+    .. math::        
+        f_1 \approx 1 - \frac{1}{2}\frac{\mu}{r_2^3} (t_1 - t_2)^2
+        
+        f_3 \approx 1 - \frac{1}{2}\frac{\mu}{r_2^3} (t_3 - t_2)^2
+        
+        g_1 \approx (t_1 - t_2) - \frac{1}{6}\frac{\mu}{r_2^3} (t_1 - t_2)^2
+        
+        g_3 \approx (t_3 - t_2) - \frac{1}{6}\frac{\mu}{r_2^3} (t_3 - t_2)^2
+        
+        \vec{v}_2 = \frac{1}{f_1 g_3 - f_3 g_1} (-f_3 \vec{r}_1 + f_1 \vec{r}_3)
+
+        
+    For more details on theory see Chapter 5 in Howard D. Curtis' "Orbital Mechanics for
+    Engineering Students". 
+    
+    Parameters
+    ----------
+    r1 : `~numpy.ndarray` (3)
+        Heliocentric position vector at time 1 in cartesian coordinates in units
+        of AU. 
+    r2 : `~numpy.ndarray` (3)
+        Heliocentric position vector at time 2 in cartesian coordinates in units
+        of AU. 
+    r3 : `~numpy.ndarray` (3)
+        Heliocentric position vector at time 3 in cartesian coordinates in units
+        of AU.
+    t1 : float 
+        Time at r1. Units of MJD or JD work or any decimal time format (one day is 1.00) as 
+        long as all times are given in the same format. 
+    t2 : float
+        Time at r2. Units of MJD or JD work or any decimal time format (one day is 1.00) as 
+        long as all times are given in the same format. 
+    t3 : float
+        Time at r3. Units of MJD or JD work or any decimal time format (one day is 1.00) as 
+        long as all times are given in the same format. 
+    
+    Returns
+    -------
+    v2 : `~numpy.ndarray` (3)
+        Velocity of object at position r2 at time t2 in units of AU per day. 
+    """
+    t21 = t2 - t1
+    t32 = t3 - t2
+    r2_mag = np.linalg.norm(r2)
+    f1, g1, f3, g3 = _calcFG(r2_mag, t32, t21)
+    return (1 / (f1 * g3 - f3 * g1)) * (-f3 * r1 + f1 * r3)
