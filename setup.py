1b3c421f4125673a88f0f7d40bf4284149acede3
Joachim Moeyens <moeyensj@gmail.com>
2020-02-05 19:26:22 -0800

    Add setuptools_scm

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
    name="thor",
-   version="0.0.1.dev0",
-   description="Tracklet-less Heliocentric Orbit Recovery",
    license="BSD 3-Clause License",
    author="Joachim Moeyens, Mario Juric",
    author_email="moeyensj@uw.edu",
+   long_description=open("README.md").read(),
+   long_description_content_type="text/markdown",
    url="https://github.com/moeyensj/thor",
    packages=["thor"],
    package_dir={"thor": "thor"},
-   package_data={"thor": ["data/*.orb",
-                             "tests/data/*"]},
-   setup_requires=["pytest-runner"],
+   package_data={"thor": ["tests/*.txt"]},
+   use_scm_version=True,
+   setup_requires=["pytest-runner", "setuptools_scm"],
    tests_require=["pytest"],
 )
