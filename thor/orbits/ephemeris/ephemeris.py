9971b5a28159912bd5caa01ccd61b8507de65223
Joachim Moeyens <moeyensj@gmail.com>
2020-03-05 13:30:45 -0800

    Add generateEphemerisUniversal

--- thor/orbits/ephemeris/ephemeris.py
+++ thor/orbits/ephemeris/ephemeris.py
@@ -1,11 +1,14 @@
 import numpy as np
+import pandas as pd
 from numba import jit
 
 from ...constants import Constants as c
+from ...coordinates import transformCoordinates
 from ..propagate import propagateUniversal
 
 __all__ = [
     "addPlanetaryAberration",
+    "generateEphemerisUniversal"
 ]
 
 MU = c.G * c.M_SUN
@@ -83,6 +86,162 @@ def addPlanetaryAberration(orbits, t0, observer_states, lt_tol=1e-10, mu=MU, max
         lts[i] = lt
         
     return corrected_orbits, corrected_t0, lts
+
+def generateEphemerisUniversal(orbits, t0_utc, observer_states, observation_times_utc, light_time=True, lt_tol=1e-10, mu=MU, max_iter=1000, tol=1e-15):
+    """
+    Generate ephemeris for orbits relative to the location of the observer. 
     
+    Parameters
+    ----------
+    orbits : `~numpy.ndarray` (N, 6)
+        Orbits for which to generate ephemeris.
+    t0_utc : `~numpy.ndarray` (N)
+        Epoch at which orbits are defined ()
+    observer_states : `~numpy.ndarray` (M, 6)
+        State of the observer (including velocity) at the time of observations.
+    observation_times_utc : `~numpy.ndarray` (M)
+        Observation times at which the observer state vectors are true.
+    light_time : bool, optional
+        Correct orbits for light travel time (planetary aberration). 
+    lt_tol : float, optional
+        Calculate aberration to within this value in time (units of days.)
+    mu : float, optional
+        Gravitational parameter (GM) of the attracting body in units of 
+        AU**3 / d**2. 
+    max_iter : int, optional
+        Maximum number of iterations over which to converge for propagation. 
+    tol : float, optional
+        Numerical tolerance to which to compute universal anomaly during propagation using the Newtown-Raphson 
+        method.     
     
+    Returns
+    -------
+    ephemeris : `~pandas.DataFrame` (M * N, 21)
+        Ephemerides for the each orbit at the desired observation times.
+        orbit_id : 
+            Index (ID) of orbits in the input orbits array.
+        mjd_utc : 
+            Time of observation in UTC. 
+        RA_deg : 
+            Heliocentric J2000 equatorial Right Ascension in degrees.
+        Dec_deg : 
+            Heliocentric J2000 equatorial Declination in degrees.
+        vRAcosDec : 
+            Heliocentric J2000 equatorial velocity in Right Ascension in degrees per day. 
+        vDec : 
+            Heliocentric J2000 equatorial velocity in Declination in degrees per day.
+        r_au : 
+            Heliocentric distance to object.
+        delta_au :
+            Topocentric distance to object. 
+        light_time : 
+            Light time travel time. 
+        obj_x : 
+            x-position of the object at the time of light emission/reflection (if the orbit 
+            was adjusted for planetary aberration.)
+        obj_y : 
+            y-position of the object at the time of light emission/reflection (if the orbit 
+            was adjusted for planetary aberration.)
+        obj_z : 
+            z-position of the object at the time of light emission/reflection (if the orbit 
+            was adjusted for planetary aberration.)
+        obj_vx : 
+            x-velocity of the object at the time of light emission/reflection (if the orbit 
+            was adjusted for planetary aberration.)
+        obj_vy : 
+            y-velocity of the object at the time of light emission/reflection (if the orbit 
+            was adjusted for planetary aberration.)
+        obj_vz : 
+            z-velocity of the object at the time of light emission/reflection (if the orbit 
+            was adjusted for planetary aberration.)
+        obs_x : 
+            x-position of the observer at the time of observation.
+        obs_y : 
+            y-position of the observer at the time of observation.
+        obs_z : 
+            z-position of the observer at the time of observation.
+        obs_vx : 
+            x-velocity of the observer at the time of observation.
+        obs_vy : 
+            y-velocity of the observer at the time of observation.
+        obs_vz : 
+            z-velocity of the observer at the time of observation.
+    """
+    # Propagate orbits to observer states
+    propagated_orbits = propagateUniversal(orbits, t0_utc, observation_times_utc, mu=mu, max_iter=max_iter, tol=tol)
+    
+    # Stack observation times and observer states (so we can add/subtract arrays later instead of looping)
+    observation_times_utc_stacked = np.hstack([observation_times_utc for i in range(len(orbits))])
+    observer_states_stacked = np.vstack([observer_states for i in range(len(orbits))])
+
+    # Add light time correction 
+    lt = np.zeros(len(propagated_orbits))
+    if light_time is True:
+        propagated_orbits_lt, t0_lt, lt = addPlanetaryAberration(
+            propagated_orbits[:, 2:], 
+            observation_times_utc_stacked, 
+            observer_states[:, :3], 
+            lt_tol=lt_tol,  
+            mu=mu, 
+            max_iter=max_iter, 
+            tol=tol
+        )
+        propagated_orbits[:, 2:] = propagated_orbits_lt
+    
+    # Calculate topocentric to target state
+    delta_state = propagated_orbits[:, 2:] - observer_states_stacked
+
+    # Convert topocentric to target state to spherical coordinates
+    # including velocities
+    state_spherical = transformCoordinates(
+        delta_state, 
+        "ecliptic", 
+        "equatorial", 
+        representation_in="cartesian", 
+        representation_out="spherical"
+    )
+
+    # Output results
+    ephemeris = np.zeros((len(orbits) * len(observation_times), 21))
+    ephemeris[:, 0] = propagated_orbits[:, 0]
+    ephemeris[:, 1] = observation_times_utc_stacked
+    ephemeris[:, 2] = state_spherical[:, 1]
+    ephemeris[:, 3] = state_spherical[:, 2]
+    ephemeris[:, 4] = state_spherical[:, 4] * np.cos(np.radians(state_spherical[:, 5]))
+    ephemeris[:, 5] = state_spherical[:, 5] 
+    ephemeris[:, 6] = np.linalg.norm(propagated_orbits[:, 2:5], axis=1)
+    ephemeris[:, 7] = np.linalg.norm(delta_state[:, :3], axis=1)
+    ephemeris[:, 8] = lt
+    ephemeris[:, 9:15] = propagated_orbits[:, 2:]
+    ephemeris[:, 15:] = observer_states_stacked
+
+    # Make a dataframe with the output results
+    ephemeris = pd.DataFrame(
+        ephemeris,
+        columns=[
+            "orbit_id",
+            "mjd_utc",
+            "RA_deg",
+            "Dec_deg",
+            "vRAcosDec",
+            "vDec",
+            "r_au",
+            "delta_au",
+            "light_time",
+            "obj_x",
+            "obj_y",
+            "obj_z",
+            "obj_vx",
+            "obj_vy",
+            "obj_vz",
+            "obs_x",
+            "obs_y",
+            "obs_z",
+            "obs_vx",
+            "obs_vy",
+            "obs_vz",
+        ]
+    )
+    ephemeris["orbit_id"] = ephemeris["orbit_id"].astype(int)
+    return ephemeris
     
