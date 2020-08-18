e22643e19dd7ba2c44ade26981697cae58c7ffe0
Joachim Moeyens <moeyensj@gmail.com>
2020-08-18 13:42:26 -0700

    Add getHorizonsElements

--- thor/utils/horizons.py
+++ thor/utils/horizons.py
@@ -3,6 +3,7 @@ from .astropy import _checkTime
 
 __all__ = [
     "getHorizonsVectors",
+    "getHorizonsElements",
     "getHorizonsEphemeris"
 ]
 
@@ -41,10 +42,50 @@ def getHorizonsVectors(obj_id, times, location="@sun", id_type="smallbody", aber
         id_type=id_type,
     )
     vectors = obj.vectors(
+        refsystem="J2000",
         refplane="ecliptic",
         aberrations=aberrations,
     ).to_pandas()
     return vectors
+
+def getHorizonsElements(obj_id, times, location="@sun", id_type="smallbody"):
+    """
+    Query JPL Horizons (through astroquery) for an object's
+    elements at the given times.
+    
+    Parameters
+    ----------
+    obj_id : str
+        Object ID / designation recognizable by HORIZONS. 
+    times : `~astropy.core.time.Time`
+        Astropy time object at which to gather state vectors.
+    location : str, optional
+        Location of the origin typically a NAIF code.
+        ('0' or '@ssb' for solar system barycenter, '10' or '@sun' for heliocenter)
+        [Default = '@sun']
+    id_type : {'majorbody', 'smallbody', 'designation', 
+               'name', 'asteroid_name', 'comet_name', 'id'}
+        ID type, Horizons will find closest match under any given type.
+        
+    Returns
+    -------
+    elements : `~pandas.DataFrame`
+        Dataframe containing the full cartesian state, r, r_rate, delta, delta_rate and light time 
+        of the object at each time. 
+    """
+    _checkTime(times, "times")
+    obj = Horizons(
+        id=obj_id, 
+        epochs=times.tdb.mjd,
+        location=location,
+        id_type=id_type,
+    )
+    elements = obj.elements(
+        refsystem="J2000",
+        refplane="ecliptic",
+        tp_type="absolute"
+    ).to_pandas()
+    return elements
 
 def getHorizonsEphemeris(obj_id, times, location, id_type="smallbody"):
     """
