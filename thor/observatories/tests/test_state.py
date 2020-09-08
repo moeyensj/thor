abaebb2437f38cf0c54631b2f41c840e17a42317
Joachim Moeyens <moeyensj@gmail.com>
2020-09-08 09:32:28 -0700

    Update getObserverState unit tests to use testOrbits

--- thor/observatories/tests/test_state.py
+++ thor/observatories/tests/test_state.py
@@ -3,78 +3,108 @@ import numpy as np
 from astropy import units as u
 from astropy.time import Time
 
-from ...utils import getHorizonsVectors
+from ...utils import testOrbits
 from ...utils import getSPICEKernels
 from ...utils import getMPCObservatoryCodes
+from ...utils import getHorizonsObserverState
 from ..state import getObserverState
 
-OBSERVATORIES = ["I11", "I41", "005", "F51", "500", "568", "W84", "012", "I40", "286"]
+OBSERVATORIES = ["I11", "005", "F51", "500", "568", "W84", "012", "I40", "286"]
 
 # Set some observation times
-TIMES = Time(np.arange(54000, 54030, 1), scale="tdb", format="mjd")
+TIMES = Time(
+    np.arange(54000, 54030, 1), 
+    scale="tdb", 
+    format="mjd"
+)
 
-def test_getObserverStateAgainstHorizons_heliocentric_ecliptic():
-    
+def test_getObserverStateAgainstHorizons_heliocentric():
+    """
+    Query Horizons (via astroquery) for heliocentric state vectors of each observatory at each observation time. 
+    Use THOR to find heliocentric state vectors of each observatory at each observation time. 
+    Compare the resulting state vectors and test how well they agree with the ones pulled from Horizons.
+    """
+    # Insure SPICE is ready for computation
     getSPICEKernels()
+
+    # Make sure the latest version of the MPC observatory codes
+    # has been downloaded
     getMPCObservatoryCodes()
+    
+    origin = "heliocenter"
+   
+    # Query Horizons for heliocentric observer states of each observatory at each time
+    horizons_states = getHorizonsObserverState(
+        OBSERVATORIES, 
+        TIMES, 
+        origin=origin,
+        aberrations="geometric",
+    )
+    horizons_states = horizons_states[["x", "y", "z", "vx", "vy", "vz"]].values
 
-    for observatory in OBSERVATORIES:
-        # Get state of observer
-        observer_states = getObserverState([observatory], TIMES, frame="ecliptic", origin="heliocenter")
-        observer_states = observer_states[["obs_x", "obs_y", "obs_z", "obs_vx", "obs_vy", "obs_vz"]].values
+    # Use THOR to find the heliocentric states for each observatory at each time
+    observer_states = getObserverState(
+        OBSERVATORIES, 
+        TIMES, 
+        origin=origin,
+    )
+    observer_states = observer_states[["obs_x", "obs_y", "obs_z", "obs_vx", "obs_vy", "obs_vz"]].values
+    
+    # Test that each state agrees with Horizons
+    # to within the tolerances below
+    testOrbits(
+        observer_states,
+        horizons_states,
+        position_tol=(20*u.m),
+        velocity_tol=(1*u.mm/u.s),
+        magnitude=True
+    )
 
-        horizons_states = getHorizonsVectors("sun", TIMES, location=observatory, id_type="majorbody")
-        horizons_states = -horizons_states[["x", "y", "z", "vx", "vy", "vz"]].values
+    return
 
-        # Calculate difference in observer position in meters for each epoch
-        r_diff = np.linalg.norm(horizons_states[:, :3] - observer_states[:, :3], axis=1) * u.AU.to(u.m)    
+def test_getObserverStateAgainstHorizons_barycentric():
+    """
+    Query Horizons (via astroquery) for barycentric state vectors of each observatory at each observation time. 
+    Use THOR to find barycentric state vectors of each observatory at each observation time. 
+    Compare the resulting state vectors and test how well they agree with the ones pulled from Horizons.
+    """
+    # Insure SPICE is ready for computation
+    getSPICEKernels()
 
-        print("Mean difference in heliocentric position vector for observatory {}: {:.3f} m".format(observatory, np.mean(r_diff)))
+    # Make sure the latest version of the MPC observatory codes
+    # has been downloaded
+    getMPCObservatoryCodes()
+    
+    origin = "barycenter"
+   
+    # Query Horizons for barycentric observer states of each observatory at each time
+    horizons_states = getHorizonsObserverState(
+        OBSERVATORIES, 
+        TIMES, 
+        origin=origin,
+        aberrations="geometric",
+    )
+    horizons_states = horizons_states[["x", "y", "z", "vx", "vy", "vz"]].values
 
-        # Assert that the heliocentric position is to within 10 meters
-        np.testing.assert_allclose(r_diff, np.zeros(len(r_diff)), atol=10., rtol=0.0)
-
-        # Calculate difference in observer velocity in mm/s for each epoch
-        v_diff = np.linalg.norm(horizons_states[:, 3:] - observer_states[:, 3:], axis=1) * (u.AU / u.d).to(u.mm / u.s)    
-
-        print("Mean difference in heliocentric velocity vector for observatory {}: {:.3f} mm/s".format(observatory, np.mean(v_diff)))
-
-        # Assert that the heliocentric position is to within 1 mm/s
-        np.testing.assert_allclose(v_diff, np.zeros(len(v_diff)), atol=1, rtol=0.0)
-
-        return
+    # Use THOR to find the barycentric states for each observatory at each time
+    observer_states = getObserverState(
+        OBSERVATORIES, 
+        TIMES, 
+        origin=origin,
+    )
+    observer_states = observer_states[["obs_x", "obs_y", "obs_z", "obs_vx", "obs_vy", "obs_vz"]].values
+    
+    # Test that each state agrees with Horizons
+    # to within the tolerances below
+    testOrbits(
+        observer_states,
+        horizons_states,
+        position_tol=(20*u.m),
+        velocity_tol=(1*u.mm/u.s),
+        magnitude=True
+    )
+    return
         
-def test_getObserverStateAgainstHorizons_barycentric_ecliptic():
-    
-    getSPICEKernels()
-    getMPCObservatoryCodes()
-
-    for observatory in OBSERVATORIES:
-        # Get state of observer
-        observer_states = getObserverState([observatory], TIMES, frame="ecliptic", origin="barycenter")
-        observer_states = observer_states[["obs_x", "obs_y", "obs_z", "obs_vx", "obs_vy", "obs_vz"]].values
-
-        horizons_states = getHorizonsVectors("ssb", TIMES, location=observatory, id_type="majorbody")
-        horizons_states = -horizons_states[["x", "y", "z", "vx", "vy", "vz"]].values
-
-        # Calculate difference in observer position in meters for each epoch
-        r_diff = np.linalg.norm(horizons_states[:, :3] - observer_states[:, :3], axis=1) * u.AU.to(u.m)    
-
-        print("Mean difference in barycentric position vector for observatory {}: {:.3f} m".format(observatory, np.mean(r_diff)))
-
-        # Assert that the barycentric position is to within 10 meters
-        np.testing.assert_allclose(r_diff, np.zeros(len(r_diff)), atol=10., rtol=0.0)
-
-        # Calculate difference in observer velocity in mm/s for each epoch
-        v_diff = np.linalg.norm(horizons_states[:, 3:] - observer_states[:, 3:], axis=1) * (u.AU / u.d).to(u.mm / u.s)    
-
-        print("Mean difference in barycentric velocity vector for observatory {}: {:.3f} mm/s".format(observatory, np.mean(v_diff)))
-
-        # Assert that the barycentric position is to within 1 mm/s
-        np.testing.assert_allclose(v_diff, np.zeros(len(v_diff)), atol=1, rtol=0.0)
-
-        return
-
 def test_getObserverState_raises():
 
     with pytest.raises(ValueError):
