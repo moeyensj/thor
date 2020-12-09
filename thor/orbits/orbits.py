38a0b99c18f01a79d8e3ba64abf2c2bfed1a4ef1
Joachim Moeyens <moeyensj@gmail.com>
2020-12-09 09:00:24 -0800

    Add split capabilities to Orbits

--- thor/orbits/orbits.py
+++ thor/orbits/orbits.py
@@ -44,22 +44,30 @@ class Orbits:
         # If object IDs have been passed make sure each orbit has one
         if ids is not None:
             assert len(ids) == self.num_orbits
-        self.ids = np.asarray(ids)
+            self.ids = np.asarray(ids)
+        else:
+            self.ids = np.arange(0, self.num_orbits, 1)
 
         # If H magnitudes have been passed make sure each orbit has one
         if H is not None:
             assert len(H) == self.num_orbits
-        self.H = np.asarray(H)
+            self.H = np.asarray(H)
+        else:
+            self.H = None
         
         # If the slope parameter G has been passed make sure each orbit has one
         if G is not None: 
             assert len(G) == self.num_orbits
-        self.G = np.asarray(G)
+            self.G = np.asarray(G)
+        else:
+            self.G = None
 
         # If covariances matrixes have been passed make sure each orbit has one
         if covariances is not None:
             assert len(covariances) == self.num_orbits
-        self.covariances = covariances
+            self.covariances = covariances
+        else:
+            self.covariances = None
 
         return
 
@@ -69,3 +77,21 @@ class Orbits:
             "Type: {}\n"
         )
         return rep.format(self.num_orbits, self.orbit_type)
+
+    def split(self, chunk_size):
+        objs = []
+        for chunk in range(0, self.num_orbits, chunk_size):
+            args = []
+            for arg in ["cartesian", "epochs"]:
+                args.append(self.__dict__[arg][chunk:chunk + chunk_size].copy())
+            
+            kwargs = {}
+            for kwarg in ["orbit_type", "ids", "H", "G", "covariances"]:
+                if type(self.__dict__[kwarg]) == np.ndarray:
+                    kwargs[kwarg] = self.__dict__[kwarg][chunk:chunk + chunk_size].copy()
+                else:
+                    kwargs[kwarg] = self.__dict__[kwarg]
+            
+            objs.append(Orbits(*args, **kwargs))
+        
+        return objs
