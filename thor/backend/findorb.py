d9b778aea6bc157ba2cf988aec6a3d5818e5aca9
Joachim Moeyens <moeyensj@gmail.com>
2020-12-11 12:44:45 -0800

    Use tempfile to create temporary find_orb working directories

--- thor/backend/findorb.py
+++ thor/backend/findorb.py
@@ -1,6 +1,7 @@
 import os
 import time
 import shutil
+import tempfile
 import warnings
 import subprocess 
 import numpy as np
@@ -58,7 +59,38 @@ class FINDORB(Backend):
                 newline="\n", 
                 fmt="%.16f"
             )
+            f.close()
         return 
+
+    def _setWorkEnv(self, temp_dir):
+
+        shutil.copytree(
+            os.path.expanduser("~/.find_orb"), 
+            os.path.join(temp_dir, ".find_orb"),
+            ignore=shutil.ignore_patterns(
+                "elements.txt",
+                "guide.txt",
+                "mpc_fmt.txt",
+                "observe.txt",
+                "residual.txt",
+                "total.json",
+                "vectors.txt",
+                "covar.txt",
+                "debug.txt", 
+                "elements.json", 
+                "elem_short.json", 
+                "combined.json",
+                "linux_p1550p2650.430t"
+            )
+        )
+        os.symlink(
+            os.path.expanduser("~/.find_orb/linux_p1550p2650.430t"), 
+            os.path.join(temp_dir, ".find_orb/linux_p1550p2650.430t")
+        )
+
+        env = os.environ.copy()
+        env["HOME"] = temp_dir
+        return env
 
     def _propagateOrbits(self, orbits, t1):
         """
@@ -69,172 +101,196 @@ class FINDORB(Backend):
 
 
         """
-        times_in_file = "times_prop_{}.in".format(os.getpid())
-        self._writeTimes(
-            times_in_file,
-            t1.tt,
-            "tt"
-        )
-
-        dfs = []
-        out_dirs = []
-        for i in range(orbits.num_orbits):
+        propagated_dfs = []
+        with tempfile.TemporaryDirectory() as temp_dir:
             
-            orbit_id = orbits.ids[i]
-            out_dir = "vectors{}".format(orbit_id)
-            vectors_txt = "{}/vectors.txt".format(out_dir)
-            out_dirs.append(out_dir)
+            # Set this environment's home directory to the temporary directory
+            # to prevent bugs with multiple processes writing to the ~/.find_orb/
+            # directory
+            env = self._setWorkEnv(temp_dir)
+            
+            times_in_file = os.path.join(temp_dir, "times_prop.in")
+            self._writeTimes(
+                times_in_file,
+                t1.tt,
+                "tt"
+            )
+            
+            for i in range(orbits.num_orbits):
+                    
+                orbit_id = orbits.ids[i]
+                out_dir = os.path.join(temp_dir, "vectors{}".format(orbit_id))
+                vectors_txt = os.path.join(out_dir, "vectors.txt")
 
-            call = [
-                "fo", 
-                "-o", 
-                "o{}".format(orbit_id), 
-                "-v",  
-                "{},{}".format(
+                # Format the state vector into the type understood by find_orb
+                fo_orbit = "-v{},{}".format(
                     orbits.epochs[i].tt.jd,
                     ",".join(orbits.cartesian[i].astype("str"))
-                ),
-                "-E",
-                "0",
-                "-C",
-                "Sun",
-                "-O",
-                out_dir,
-                "-e",
-                vectors_txt,
-                "-D",
-                self.config_file, 
-                "EPHEM_STEP_SIZE=t{}".format(times_in_file)
+                )
 
-            ]
-            subprocess.call(call)
-            
-            wait = 0
-            while not os.path.exists(vectors_txt):
-                time.sleep(0.1)
-                wait += 1
-                if wait == 10:
-                    break
+                call = [
+                    "fo", 
+                    "-o", 
+                    "o{}".format(orbit_id), 
+                    fo_orbit,
+                    "-E",
+                    "0",
+                    "-C",
+                    "Sun",
+                    "-O",
+                    out_dir,
+                    "-e",
+                    vectors_txt,
+                    "-D",
+                    self.config_file, 
+                    "EPHEM_STEP_SIZE=t{}".format(times_in_file)
+                ]
+                ret = subprocess.run(
+                    call, 
+                    shell=False, 
+                    env=env, 
+                    check=False, 
+                    capture_output=True
+                )
+                
+                if (os.path.exists(vectors_txt)):
+                    df = pd.read_csv(
+                        vectors_txt,
+                        header=0,
+                        delim_whitespace=True,
+                        names=["jd_tt", "x", "y", "z", "vx", "vy", "vz"]
+                    )
+                    df["orbit_id"] = [i for _ in range(len(df))]
+                else:
+                    df = pd.DataFrame(
+                        columns=["orbit_id", "jd_tt", "x", "y", "z", "vx", "vy", "vz"]
+                    )
 
-            # Read results of propagation                
-            df = pd.read_csv(
-                vectors_txt,
-                header=0,
-                delim_whitespace=True,
-                names=["jd_tt", "x", "y", "z", "vx", "vy", "vz"]
+                #result["name"] = trkSub
+                #result["findorb"] = {
+                #    'args': ret.args,
+                #    'returncode': ret.returncode,
+                #    'stdout': ret.stdout.decode('utf-8'),
+                #    'stderr': ret.stderr.decode('utf-8')
+                #}
+                #print(ret.stdout.decode('utf-8'))
+                #print(ret.stderr.decode('utf-8'))
+                propagated_dfs.append(df)
+
+            propagated = pd.concat(propagated_dfs)
+            propagated.reset_index(
+                inplace=True,
+                drop=True
             )
-            df["orbit_id"] = [i for _ in range(len(df))]
+            propagated["epoch_mjd_tdb"] = Time(
+                propagated["jd_tt"].values,
+                scale="tt",
+                format="jd"
+            ).tdb.mjd
+            propagated = propagated[["orbit_id", "epoch_mjd_tdb", "x", "y", "z", "vx", "vy", "vz"]]
 
-            dfs.append(df)
-
-        propagated = pd.concat(dfs)
-        propagated.reset_index(
-            inplace=True,
-            drop=True
-        )
-        propagated["epoch_mjd_tdb"] = Time(
-            propagated["jd_tt"].values,
-            scale="tt",
-            format="jd"
-        ).tdb.mjd
-        propagated = propagated[["orbit_id", "epoch_mjd_tdb", "x", "y", "z", "vx", "vy", "vz"]]
-
-        if self.remove_files:
-            os.remove(times_in_file)
-            for out_dir in out_dirs:
-                shutil.rmtree(out_dir)
-
-        if orbits.ids is not None:
-            propagated["orbit_id"] = orbits.ids[propagated["orbit_id"].values]
+            if orbits.ids is not None:
+                propagated["orbit_id"] = orbits.ids[propagated["orbit_id"].values]
 
         return propagated
 
     def _generateEphemeris(self, orbits, observers):
 
         ephemeris_dfs = []
-        for observatory_code, observation_times in observers.items():
+        with tempfile.TemporaryDirectory() as temp_dir:
             
-            # Write the observation times to a file
-            times_in_file = "times_eph_{}.in".format(os.getpid())
-            self._writeTimes(
-                times_in_file,
-                observation_times.utc,
-                "utc"
-            )
-
-            # Certain observatories are dealt with differently, so appropriately
-            # define the columns
-            if (observatory_code == "500"):
-                columns = [
-                    "jd_utc", "RA_deg", "Dec_deg", "delta_au", "r_au",
-                    "elong", "ph_ang", "mag",  
-                    "'/hr", "PA", "rvel", 
-                    "lon", "lat", "altitude_km"
-                ] 
-            else:
-                columns = [
-                    "jd_utc", "RA_deg", "Dec_deg", "delta_au", "r_au",
-                    "elong", "ph_ang", "mag",  
-                    "RA_'/hr", "dec_'/hr",  
-                    "alt",  "az",  "rvel",   
-                    "lon", "lat", "altitude_km"
-                ] 
-
-            out_dirs = []
-            # For each orbit calculate their ephemerides
-            for i in range(orbits.num_orbits):
-                
-                # Set directory variables
-                orbit_id = orbits.ids[i]
-                out_dir = "ephemeris{}_{}".format(orbit_id, observatory_code)
-                ephemeris_txt = "{}/ephemeris.txt".format(out_dir)
-                out_dirs.append(out_dir)
-
-                # Call fo and generate ephemerides
-                call = [
-                    "fo", 
-                    "-o", 
-                    "o{}".format(orbit_id), 
-                    "-v",  
-                    "{},{},H=20.0".format(
-                        orbits.epochs[i].tt.jd,
-                        ",".join(orbits.cartesian[i].astype("str")),
-                    ),
-                    "-C",
-                    "{}".format(observatory_code),
-                    "-O",
-                    out_dir,
-                    "-e",
-                    ephemeris_txt,
-                    "-D",
-                    self.config_file,
-                    "EPHEM_STEP_SIZE=t{}".format(times_in_file),   
-                ]
-                subprocess.call(call)
-
-                wait = 0
-                while not os.path.exists(ephemeris_txt):
-                    time.sleep(0.1)
-                    wait += 1
-                    if wait == 10:
-                        break
-                
-                # Read the resulting ephemeris file
-                ephemeris = pd.read_csv(
-                    ephemeris_txt,
-                    header=0,
-                    delim_whitespace=True,
-                    names=columns
-
+            # Set this environment's home directory to the temporary directory
+            # to prevent bugs with multiple processes writing to the ~/.find_orb/
+            # directory
+            env = self._setWorkEnv(temp_dir)
+            
+            for observatory_code, observation_times in observers.items():
+            
+                times_in_file = os.path.join(temp_dir, "times_eph.in")
+                self._writeTimes(
+                    times_in_file,
+                    observation_times.tt,
+                    "tt"
                 )
-                ephemeris["orbit_id"] = [i for _ in range(len(ephemeris))]
-                ephemeris["observatory_code"] = [observatory_code for _ in range(len(ephemeris))]
-                ephemeris_dfs.append(ephemeris)
 
-            if self.remove_files:
-                os.remove(times_in_file)
-                for out_dir in out_dirs:
-                    shutil.rmtree(out_dir)
+                # Certain observatories are dealt with differently, so appropriately
+                # define the columns
+                if (observatory_code == "500"):
+                    columns = [
+                        "jd_utc", "RA_deg", "Dec_deg", "delta_au", "r_au",
+                        "elong", "ph_ang", "mag",  
+                        "'/hr", "PA", "rvel", 
+                        "lon", "lat", "altitude_km"
+                    ] 
+                else:
+                    columns = [
+                        "jd_utc", "RA_deg", "Dec_deg", "delta_au", "r_au",
+                        "elong", "ph_ang", "mag",  
+                        "RA_'/hr", "dec_'/hr",  
+                        "alt",  "az",  "rvel",   
+                        "lon", "lat", "altitude_km"
+                    ] 
+
+                # For each orbit calculate their ephemerides
+                for i in range(orbits.num_orbits):
+
+                    # Set directory variables
+                    orbit_id = orbits.ids[i]
+                    out_dir = os.path.join(temp_dir, "ephemeris{}_{}".format(orbit_id, observatory_code))
+                    ephemeris_txt = os.path.join(out_dir, "ephemeris.txt")
+                    
+                    # Format the state vector into the type understood by find_orb
+                    fo_orbit = "-v{},{}".format(
+                        orbits.epochs[i].tt.jd,
+                        ",".join(orbits.cartesian[i].astype("str"))
+                    )
+                    if orbits.H is not None:
+                        fo_orbit += ",H={}".format(orbits.H[i])
+
+                    # Call fo and generate ephemerides
+                    call = [
+                        "fo", 
+                        "-o", 
+                        "o{}".format(orbit_id), 
+                        fo_orbit,
+                        "-C",
+                        "{}".format(observatory_code),
+                        "-O",
+                        out_dir,
+                        "-e",
+                        ephemeris_txt,
+                        "-D",
+                        self.config_file,
+                        "EPHEM_STEP_SIZE=t{}".format(times_in_file),   
+                        "JSON_EPHEM_NAME={}".format(os.path.join(temp_dir, "eph%p_%c.json")),
+                        "JSON_ELEMENTS_NAME={}".format(os.path.join(temp_dir, "ele%p.json")),
+                        "JSON_SHORT_ELEMENTS={}".format(os.path.join(temp_dir, "short%p.json")),
+                        "JSON_COMBINED_NAME={}".format(os.path.join(temp_dir, "com%p_%c.json"))
+                    ]
+                    ret = subprocess.run(
+                        call, 
+                        shell=False, 
+                        env=env, 
+                        check=False, 
+                        capture_output=True
+                    )
+                    
+                    if (os.path.exists(ephemeris_txt)):
+                        ephemeris = pd.read_csv(
+                            ephemeris_txt,
+                            header=0,
+                            delim_whitespace=True,
+                            names=columns
+
+                        )
+                        ephemeris["orbit_id"] = [i for _ in range(len(ephemeris))]
+                        ephemeris["observatory_code"] = [observatory_code for _ in range(len(ephemeris))]
+                    else:
+                        ephemeris = pd.DataFrame(
+                            columns=[["orbit_id", "observatory_code"] + columns]
+                        )
+
+                    ephemeris_dfs.append(ephemeris)
 
         # Combine ephemeris data frames and sort by orbit ID,
         # observatory code and observation time, then reset the
