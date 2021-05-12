810c4410ec3a9f66639a731a02d186b67b679fc8
Joachim Moeyens <moeyensj@gmail.com>
2021-05-12 15:29:35 -0700

    Remove file name from KERNELS dict, add DE430 to SPK kernel name

--- thor/utils/spice.py
+++ thor/utils/spice.py
@@ -13,43 +13,28 @@ __all__ = [
 ]
 
 KERNELS = {
-        # Internal Name : [File, URL]
-        "LSK - Latest" : [
-            "latest_leapseconds.tls", 
-            "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/latest_leapseconds.tls"
-        ],
-        "Planetary Constants" : [
-            "pck00010.tpc",
-            "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc"
-        ],
-        "Earth PCK - Latest High Accuracy" : [
-            "earth_latest_high_prec.bpc",
-            "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc"
-        ],
-        "Earth PCK - Historical High Accuracy" : [
-            "earth_720101_070426.bpc",
-            "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_720101_070426.bpc"
-        ],
-        "Earth PCK - Long Term Predict Low Accuracy" : [
-            "earth_200101_990628_predict.bpc",
-            "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_200101_990628_predict.bpc"
-        ],
-        "Earth FK" : [
-            "earth_assoc_itrf93.tf",
-            "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/fk/planets/earth_assoc_itrf93.tf"
-        ],
-        "Planetary SPK" : [
-            "de430.bsp", 
-            "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de430.bsp" 
-        ]
-    }
+    # Internal Name :  URL
+    "LSK - Latest" : "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/latest_leapseconds.tls",
+    "Planetary Constants" : "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc",
+    "Earth PCK - Latest High Accuracy" : "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc",
+    "Earth PCK - Historical High Accuracy" : "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_720101_070426.bpc",
+    "Earth PCK - Long Term Predict Low Accuracy" : "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_200101_990628_predict.bpc",
+    "Earth FK" : "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/fk/planets/earth_assoc_itrf93.tf",
+    "Planetary SPK - DE430" : "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de430.bsp",
+}
 
-def getSPICEKernels(kernels=["LSK - Latest",  
-                             "Planetary Constants", 
-                             "Earth PCK - Long Term Predict Low Accuracy",
-                             "Earth PCK - Historical High Accuracy", 
-                             "Earth PCK - Latest High Accuracy", 
-                             "Planetary SPK"]):
+BASEKERNELS = [
+    "LSK - Latest",  
+    "Planetary Constants", 
+    "Earth PCK - Long Term Predict Low Accuracy",
+    "Earth PCK - Historical High Accuracy", 
+    "Earth PCK - Latest High Accuracy", 
+]
+KERNELS_DE430 = BASEKERNELS + ["Planetary SPK - DE430"]
+
+def getSPICEKernels(
+        kernels=KERNELS_DE430
+    ):
     """
     Download SPICE kernels. If any already exist, check if they have been updated. If so, replace the 
     outdated file with the latest version. 
@@ -59,7 +44,7 @@ def getSPICEKernels(kernels=["LSK - Latest",
     "Earth PCK - Latest High Accuracy": earth_latest_high_prec.bpc downloaded from https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck,
     "Earth PCK - Historical High Accuracy": earth_720101_070426.bpc downloaded from https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck,
     "Earth PCK - Long Term Predict Low Accuracy": earth_070425_370426_predict.bpc downloaded from https://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/
-    "Planetary SPK": de430.bsp downloaded from https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/
+    "Planetary SPK - DE430": de430.bsp downloaded from https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/
     
     Only the leapsecond and Earth planetary constants kernels are checked for updates since these files are rather small (< 10 MB). The 
     planetary ephemerides file is over 1.5 GB and is not checked for an update (these files are not updated regularly and are often released as 
@@ -74,7 +59,7 @@ def getSPICEKernels(kernels=["LSK - Latest",
             "Earth PCK - Latest High Accuracy"
             "Earth PCK - Historical High Accuracy"
             "Earth PCK - Long Term Predict Low Accuracy"
-            "Planetary SPK"
+            "Planetary SPK - DE430"
     
     Returns
     -------
@@ -82,15 +67,12 @@ def getSPICEKernels(kernels=["LSK - Latest",
     """
     for kernel in kernels:
         logger.info("Checking for {} kernel...".format(kernel))
-        _downloadFile(os.path.join(os.path.dirname(__file__), "..", "data"), KERNELS[kernel][1])
+        url = KERNELS[kernel]
+        _downloadFile(os.path.join(os.path.dirname(__file__), "..", "data"), url)
     return
 
-def setupSPICE(kernels=["LSK - Latest",  
-                        "Planetary Constants", 
-                        "Earth PCK - Long Term Predict Low Accuracy",
-                        "Earth PCK - Historical High Accuracy", 
-                        "Earth PCK - Latest High Accuracy", 
-                        "Planetary SPK"]
+def setupSPICE(
+        kernels=KERNELS_DE430
     ):
     """
     Loads the leapsecond, the Earth planetary constants and the planetary ephemerides kernels into SPICE. 
@@ -118,7 +100,7 @@ def setupSPICE(kernels=["LSK - Latest",
         logger.info("Enabling SPICE...")
         log = _readFileLog(os.path.join(os.path.dirname(__file__), "..", "data/log.yaml"))
         for kernel in kernels:
-            file_name = KERNELS[kernel][0]
+            file_name = os.path.basename(KERNELS[kernel])
             if file_name not in log.keys():
                 err = ("{} not found. Please run thor.utils.getSPICEKernels to download SPICE kernels.")
                 raise FileNotFoundError(err.format(file_name))
