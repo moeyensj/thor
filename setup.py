5b5ec8f07906aa514f2a7c2a759df8ca1ef513ae
Spencer Nelson <swnelson@uw.edu>
2021-05-24 14:03:13 -0700

    Revert setup changes which cythonized code

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
-import numpy as np
 from setuptools import setup
-from Cython.Build import cythonize
 
 setup(
     use_scm_version={
         "write_to": "thor/version.py",
         "write_to_template": "__version__ = '{version}'",
-    },
-    ext_modules=cythonize("thor/clusters.pyx", gdb_debug=True),
-    include_dirs=[np.get_include()],
-    zip_safe=False,
+    }
 )
