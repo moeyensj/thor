e354c6abfa7b14e57d9006fa21c1ade39d96f36a
Spencer Nelson <swnelson@uw.edu>
2021-06-22 14:53:05 -0700

    Encapsulate RabbitMQ API with a connection object

--- thorctl/tasks.py
+++ thorctl/tasks.py
@@ -1,10 +1,11 @@
 import dataclasses
 import json
 import uuid
+import pika
 
 
 @dataclasses.dataclass
-class Task:
+class TaskPayload:
     job_id: str
     task_id: str
     input_data_uri: str
@@ -20,11 +21,43 @@ class Task:
 
     @classmethod
     def from_str(cls, s: str):
-        return Task(**json.loads(s))
+        return cls(**json.loads(s))
 
     @classmethod
     def from_bytes(cls, b: bytes):
         return cls.from_str(b.decode("utf8"))
+
+
+class Task:
+    def __init__(
+            self,
+            channel: pika.channel.Channel,
+            delivery_tag: int,
+            payload: TaskPayload):
+        self._channel = channel
+        self._delivery_tag = delivery_tag
+        self.payload = payload
+
+    @classmethod
+    def from_msg(cls,
+                 channel: pika.channel.Channel,
+                 method: pika.amqp_object.Method,
+                 properties: pika.amqp_object.Properties,
+                 body: bytes) -> "Task":
+        return Task(
+            channel=channel,
+            delivery_tag=method.delivery_tag,
+            payload=TaskPayload.from_bytes(body)
+        )
+
+    def mark_success(self):
+        self._channel.basic_ack(delivery_tag=self._delivery_tag)
+
+    def mark_failed(self, requeue: bool = False):
+        self._channel.basic_nack(
+            delivery_tag=self._delivery_tag,
+            requeue=requeue,
+        )
 
 
 def new_task_id():
