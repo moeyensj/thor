5d42b896154f3a3c2ea20e69aa17e168edfe593c
Spencer Nelson <swnelson@uw.edu>
2021-07-07 22:21:23 -0700

    Launch on a small instance

--- infra/autoscaler/launch.sh
+++ infra/autoscaler/launch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Bringing down existing autoscaler..."
+gcloud compute instances delete thor-autoscaler
+echo "Bringing up new one..."
 gcloud compute instances create thor-autoscaler \
        --create-disk="image-family=thor-autoscaler,boot=yes,size=100GB,auto-delete=yes" \
-       --service-account="thor-autoscaler" \
+       --service-account="thor-autoscaler@moeyens-thor-dev.iam.gserviceaccount.com" \
        --scopes=cloud-platform \
        --zone=us-west1-a \
+       --machine-type=e2-small
