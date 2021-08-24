4c049c3ec3cc9bbb9d4ad719ca4ae3581f8c8389
Joachim Moeyens <moeyensj@gmail.com>
2021-08-24 09:46:38 -0700

    Add selectTestOrbits

--- thor/__init__.py
+++ thor/__init__.py
@@ -11,6 +11,7 @@ from .orbit import *
 from .cell import *
 from .plotting import *
 from .data_processing import *
+from .orbit_selection import *
 from .filter_orbits import *
 from .main import *
 from .analysis import *
