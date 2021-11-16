8c7705a8cd6f8aa69973e96c9437452fb425fa45
Joachim Moeyens <moeyensj@gmail.com>
2021-11-16 15:28:36 -0800

    Add times_to_df and times_from_df

--- thor/utils/astropy.py
+++ thor/utils/astropy.py
@@ -1,10 +1,65 @@
+import pandas as pd
 from astropy.time import Time
 
 __all__ = [
-    "_checkTime"
+    "times_from_df",
+    "times_to_df",
+    "_check_times"
 ]
 
-def _checkTime(time, arg_name):
+def times_from_df(df: pd.DataFrame) -> Time:
+    """
+    Read times from a `~pandas.DataFrame`.
+
+    Parameters
+    ----------
+    df : `~pandas.DataFrame`
+        DataFrame containing times.
+
+    Returns
+    -------
+    times : `~astropy.time.core.Time`
+        Astropy time object containing times read from dataframe.
+    """
+    time_col = None
+    cols = [f"mjd_{s}" for s in Time.SCALES]
+    for col in cols:
+        if col in df.columns:
+            time_col = col
+            format, scale = time_col.split("_")
+            break
+
+    times = Time(
+        df[time_col].values,
+        format=format,
+        scale=scale
+    )
+    return times
+
+def times_to_df(times: Time, time_scale: str = "utc") -> pd.DataFrame:
+    """
+    Store times as a `~pandas.DataFrame`.
+
+    Parameters
+    ----------
+    times : `~astropy.time.core.Time`
+        Astropy time object.
+    time_scale : str
+        Store times with this time scale.
+
+    Returns
+    -------
+    df : `~pandas.DataFrame`
+        DataFrame containing times.
+    """
+    data = {}
+    time = deepcopy(times)
+    time._set_scale(time_scale)
+    data[f"mjd_{time.scale}"] = time.mjd
+
+    return pd.DataFrame(data)
+
+def _check_times(times, arg_name="times"):
     """
     Check that 'time' is an astropy time object, if not, raise an error.
 
@@ -29,6 +84,6 @@ def _checkTime(time, arg_name):
         "from astropy.time import Time\n"
         "times = Time(t_array, scale='...', format='...')"
     )
-    if type(time) != Time:
+    if type(times) != Time:
         raise TypeError(err.format(arg_name))
     return
