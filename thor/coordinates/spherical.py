a2ec28b467555205c57c3a8cab62b5711fd644e8
Joachim Moeyens <moeyensj@gmail.com>
2021-12-08 14:19:27 -0800

    Enable covariance transformations for spherical coordinates

--- thor/coordinates/spherical.py
+++ thor/coordinates/spherical.py
@@ -1,15 +1,17 @@
-import warnings
 import numpy as np
 import jax.numpy as jnp
+from jax.experimental import loops
 from jax import config, jit
 from astropy.time import Time
 from astropy import units as u
-from typing import Optional
+from typing import Optional, Union
 
 config.update("jax_enable_x64", True)
+config.update('jax_platform_name', 'cpu')
 
 from .coordinates import Coordinates
 from .cartesian import CartesianCoordinates
+from .covariances import transform_covariances_jacobian
 
 __all__ = [
     "_cartesian_to_spherical",
@@ -21,54 +23,42 @@ SPHERICAL_COLS = ["rho", "lon", "lat", "vrho", "vlon", "vlat"]
 SPHERICAL_UNITS = [u.au, u.degree, u.degree, u.au / u.d, u.degree / u.d, u.degree / u.d]
 
 @jit
-def _cartesian_to_spherical(coords_cartesian):
+def _cartesian_to_spherical(coords_cartesian: Union[np.ndarray, jnp.ndarray]) -> jnp.ndarray:
     """
-    Convert spherical coordinates to cartesian coordinates.
+    Convert a single Cartesian coordinate to a spherical coordinate.
 
     Parameters
     ----------
-    x : `~numpy.ndarray` (N)
-        x-position in units of distance.
-    y : `~numpy.ndarray` (N)
-        y-position in units of distance.
-    z : `~numpy.ndarray` (N)
-        z-position in units of distance.
-    vx : `~numpy.ndarray` (N)
-        x-velocity in the same units of x per arbitrary unit
-        of time.
-    vy : `~numpy.ndarray` (N)
-        y-velocity in the same units of y per arbitrary unit
-        of time.
-    vz : `~numpy.ndarray` (N)
-        z-velocity in the same units of z per arbitrary unit
-        of time.
+    coords_cartesian : {`~numpy.ndarray`, `~jax.numpy.ndarray`} (6)
+        3D Cartesian coordinate including time derivatives.
+        x : x-position in units of distance.
+        y : y-position in units of distance.
+        z : z-position in units of distance.
+        vx : x-velocity in the same units of x per arbitrary unit of time.
+        vy : y-velocity in the same units of y per arbitrary unit of time.
+        vz : z-velocity in the same units of z per arbitrary unit of time.
 
     Returns
     -------
-    rho : `~numpy.ndarray` (N)
-        Radial distance in the same units of x, y, and z.
-    lon : `~numpy.ndarray` (N)
-        Longitude ranging from 0 to 2 pi radians.
-    lat : `~numpy.ndarray` (N)
-        Latitude ranging from -pi/2 to pi/2 radians with 0 at the
-        equator.
-    vrho : `~numpy.ndarray` (N)
-        Radial velocity in radians per arbitrary unit of time (same
-        unit of time as the x, y, and z velocities).
-    vlon : `~numpy.ndarray` (N)
-        Longitudinal velocity in radians per arbitrary unit of time
-        (same unit of time as the x, y, and z velocities).
-    vlat : `~numpy.ndarray` (N)
-        Latitudinal velocity in radians per arbitrary unit of time.
-        (same unit of time as the x, y, and z velocities).
+    coords_spherical : {`~numpy.ndarray`, `~jax.numpy.ndarray`} (6)
+        3D Spherical coordinate including time derivatives.
+        rho : Radial distance in the same units of x, y, and z.
+        lon : Longitude ranging from 0.0 to 360.0 degrees.
+        lat : Latitude ranging from -90.0 to 90.0 degrees with 0 at the equator.
+        vrho : Radial velocity in the same units as rho per arbitrary unit of time
+            (same unit of time as the x, y, and z velocities).
+        vlon : Longitudinal velocity in degrees per arbitrary unit of time
+            (same unit of time as the x, y, and z velocities).
+        vlat :Latitudinal velocity in degrees per arbitrary unit of time.
+            (same unit of time as the x, y, and z velocities).
     """
-    coords_spherical = jnp.zeros_like(coords_cartesian, dtype=jnp.float64)
-    x = coords_cartesian[:, 0]
-    y = coords_cartesian[:, 1]
-    z = coords_cartesian[:, 2]
-    vx = coords_cartesian[:, 3]
-    vy = coords_cartesian[:, 4]
-    vz = coords_cartesian[:, 5]
+    coords_spherical = jnp.zeros(6, dtype=jnp.float64)
+    x = coords_cartesian[0]
+    y = coords_cartesian[1]
+    z = coords_cartesian[2]
+    vx = coords_cartesian[3]
+    vy = coords_cartesian[4]
+    vz = coords_cartesian[5]
 
     rho = jnp.sqrt(x**2 + y**2 + z**2)
     lon = jnp.arctan2(y, x)
@@ -80,64 +70,97 @@ def _cartesian_to_spherical(coords_cartesian):
     vlon = (vy * x - vx * y) / (x**2 + y**2)
     vlat = (vz - vrho * z / rho) / jnp.sqrt(x**2 + y**2)
 
-    coords_spherical = coords_spherical.at[:, 0].set(rho)
-    coords_spherical = coords_spherical.at[:, 1].set(jnp.degrees(lon))
-    coords_spherical = coords_spherical.at[:, 2].set(jnp.degrees(lat))
-    coords_spherical = coords_spherical.at[:, 3].set(vrho)
-    coords_spherical = coords_spherical.at[:, 4].set(jnp.degrees(vlon))
-    coords_spherical = coords_spherical.at[:, 5].set(jnp.degrees(vlat))
+    coords_spherical = coords_spherical.at[0].set(rho)
+    coords_spherical = coords_spherical.at[1].set(jnp.degrees(lon))
+    coords_spherical = coords_spherical.at[2].set(jnp.degrees(lat))
+    coords_spherical = coords_spherical.at[3].set(vrho)
+    coords_spherical = coords_spherical.at[4].set(jnp.degrees(vlon))
+    coords_spherical = coords_spherical.at[5].set(jnp.degrees(vlat))
 
     return coords_spherical
 
 @jit
-def _spherical_to_cartesian(coords_spherical):
+def cartesian_to_spherical(coords_cartesian: Union[np.ndarray, jnp.ndarray]) -> jnp.ndarray:
     """
-    Convert spherical coordinates to cartesian coordinates.
+    Convert Cartesian coordinates to a spherical coordinates.
 
     Parameters
     ----------
-    rho : `~numpy.ndarray` (N)
-        Radial distance in units of distance.
-    lon : `~numpy.ndarray` (N)
-        Longitude ranging from 0 to 2 pi radians.
-    lat : `~numpy.ndarray` (N)
-        Latitude ranging from -pi/2 to pi/2 radians with 0 at the
-        equator.
-    vrho : `~numpy.ndarray` (N)
-        Radial velocity in radians per arbitrary unit of time.
-    vlon : `~numpy.ndarray` (N)
-        Longitudinal velocity in radians per arbitrary unit of time.
-    vlat : `~numpy.ndarray` (N)
-        Latitudinal velocity in radians per arbitrary unit of time.
+    coords_cartesian : {`~numpy.ndarray`, `~jax.numpy.ndarray`} (N, 6)
+        3D Cartesian coordinates including time derivatives.
+        x : x-position in units of distance.
+        y : y-position in units of distance.
+        z : z-position in units of distance.
+        vx : x-velocity in the same units of x per arbitrary unit of time.
+        vy : y-velocity in the same units of y per arbitrary unit of time.
+        vz : z-velocity in the same units of z per arbitrary unit of time.
 
     Returns
     -------
-    x : `~numpy.ndarray` (N)
-        x-position in the same units of rho.
-    y : `~numpy.ndarray` (N)
-        y-position in the same units of rho.
-    z : `~numpy.ndarray` (N)
-        z-position in the same units of rho.
-    vx : `~numpy.ndarray` (N)
-        x-velocity in the same units of rho per unit of time
-        (same unit of time as the rho, lon, and lat velocities,
-        for example, radians per day > AU per day).
-    vy : `~numpy.ndarray` (N)
-        y-velocity in the same units of rho per unit of time
-        (same unit of time as the rho, lon, and lat velocities,
-        for example, radians per day > AU per day).
-    vz : `~numpy.ndarray` (N)
-        z-velocity in the same units of rho per unit of time
-        (same unit of time as the rho, lon, and lat velocities,
-        for example, radians per day > AU per day).
+    coords_spherical : {`~numpy.ndarray`, `~jax.numpy.ndarray`} (N, 6)
+        3D Spherical coordinates including time derivatives.
+        rho : Radial distance in the same units of x, y, and z.
+        lon : Longitude ranging from 0.0 to 360.0 degrees.
+        lat : Latitude ranging from -90.0 to 90.0 degrees with 0 at the equator.
+        vrho : Radial velocity in the same units as rho per arbitrary unit of time
+            (same unit of time as the x, y, and z velocities).
+        vlon : Longitudinal velocity in degrees per arbitrary unit of time
+            (same unit of time as the x, y, and z velocities).
+        vlat : Latitudinal velocity in degrees per arbitrary unit of time.
+            (same unit of time as the x, y, and z velocities).
     """
-    coords_cartesian = jnp.zeros_like(coords_spherical, dtype=jnp.float64)
-    rho = coords_spherical[:, 0]
-    lon = jnp.radians(coords_spherical[:, 1])
-    lat = jnp.radians(coords_spherical[:, 2])
-    vrho = coords_spherical[:, 3]
-    vlon = jnp.radians(coords_spherical[:, 4])
-    vlat = jnp.radians(coords_spherical[:, 5])
+    with loops.Scope() as s:
+        N = len(coords_cartesian)
+        s.arr = jnp.zeros((N, 6), dtype=jnp.float64)
+
+        for i in s.range(s.arr.shape[0]):
+            s.arr = s.arr.at[i].set(
+                _cartesian_to_spherical(
+                    coords_cartesian[i],
+                )
+            )
+
+        coords_spherical = s.arr
+
+    return coords_spherical
+
+@jit
+def _spherical_to_cartesian(coords_spherical: Union[np.ndarray, jnp.ndarray]) -> jnp.ndarray:
+    """
+    Convert a single spherical coordinate to a Cartesian coordinate.
+
+    Parameters
+    ----------
+    coords_spherical : {`~numpy.ndarray`, `~jax.numpy.ndarray`} (6)
+        3D Spherical coordinate including time derivatives.
+        rho : Radial distance in the same units of x, y, and z.
+        lon : Longitude ranging from 0.0 to 360.0 degrees.
+        lat : Latitude ranging from -90.0 to 90.0 degrees with 0 at the equator.
+        vrho : Radial velocity in the same units as rho per arbitrary unit of time
+            (same unit of time as the x, y, and z velocities).
+        vlon : Longitudinal velocity in degrees per arbitrary unit of time
+            (same unit of time as the x, y, and z velocities).
+        vlat : Latitudinal velocity in degrees per arbitrary unit of time.
+            (same unit of time as the x, y, and z velocities).
+
+    Returns
+    -------
+    coords_cartesian : {`~numpy.ndarray`, `~jax.numpy.ndarray`} (6)
+        3D Cartesian coordinate including time derivatives.
+        x : x-position in units of distance.
+        y : y-position in units of distance.
+        z : z-position in units of distance.
+        vx : x-velocity in the same units of x per arbitrary unit of time.
+        vy : y-velocity in the same units of y per arbitrary unit of time.
+        vz : z-velocity in the same units of z per arbitrary unit of time.
+    """
+    coords_cartesian = jnp.zeros(6, dtype=jnp.float64)
+    rho = coords_spherical[0]
+    lon = jnp.radians(coords_spherical[1])
+    lat = jnp.radians(coords_spherical[2])
+    vrho = coords_spherical[3]
+    vlon = jnp.radians(coords_spherical[4])
+    vlat = jnp.radians(coords_spherical[5])
 
     cos_lat = jnp.cos(lat)
     sin_lat = jnp.sin(lat)
@@ -152,12 +175,57 @@ def _spherical_to_cartesian(coords_spherical):
     vy = cos_lat * sin_lon * vrho + rho * cos_lat * cos_lon * vlon - rho * sin_lat * sin_lon * vlat
     vz = sin_lat * vrho + rho * cos_lat * vlat
 
-    coords_cartesian = coords_cartesian.at[:, 0].set(x)
-    coords_cartesian = coords_cartesian.at[:, 1].set(y)
-    coords_cartesian = coords_cartesian.at[:, 2].set(z)
-    coords_cartesian = coords_cartesian.at[:, 3].set(vx)
-    coords_cartesian = coords_cartesian.at[:, 4].set(vy)
-    coords_cartesian = coords_cartesian.at[:, 5].set(vz)
+    coords_cartesian = coords_cartesian.at[0].set(x)
+    coords_cartesian = coords_cartesian.at[1].set(y)
+    coords_cartesian = coords_cartesian.at[2].set(z)
+    coords_cartesian = coords_cartesian.at[3].set(vx)
+    coords_cartesian = coords_cartesian.at[4].set(vy)
+    coords_cartesian = coords_cartesian.at[5].set(vz)
+
+    return coords_cartesian
+
+@jit
+def spherical_to_cartesian(coords_spherical: Union[np.ndarray, jnp.ndarray]) -> jnp.ndarray:
+    """
+    Convert spherical coordinates to Cartesian coordinates.
+
+    Parameters
+    ----------
+    coords_spherical : {`~numpy.ndarray`, `~jax.numpy.ndarray`} (N, 6)
+        3D Spherical coordinates including time derivatives.
+        rho : Radial distance in the same units of x, y, and z.
+        lon : Longitude ranging from 0.0 to 360.0 degrees.
+        lat : Latitude ranging from -90.0 to 90.0 degrees with 0 at the equator.
+        vrho : Radial velocity in the same units as rho per arbitrary unit of time
+            (same unit of time as the x, y, and z velocities).
+        vlon : Longitudinal velocity in degrees per arbitrary unit of time
+            (same unit of time as the x, y, and z velocities).
+        vlat :Latitudinal velocity in degrees per arbitrary unit of time.
+            (same unit of time as the x, y, and z velocities).
+
+    Returns
+    -------
+    coords_cartesian : {`~numpy.ndarray`, `~jax.numpy.ndarray`} (N, 6)
+        3D Cartesian coordinates including time derivatives.
+        x : x-position in units of distance.
+        y : y-position in units of distance.
+        z : z-position in units of distance.
+        vx : x-velocity in the same units of x per arbitrary unit of time.
+        vy : y-velocity in the same units of y per arbitrary unit of time.
+        vz : z-velocity in the same units of z per arbitrary unit of time.
+    """
+    with loops.Scope() as s:
+        N = len(coords_spherical)
+        s.arr = jnp.zeros((N, 6), dtype=jnp.float64)
+
+        for i in s.range(s.arr.shape[0]):
+            s.arr = s.arr.at[i].set(
+                _spherical_to_cartesian(
+                    coords_spherical[i],
+                )
+            )
+
+        coords_cartesian = s.arr
 
     return coords_cartesian
 
@@ -243,11 +311,17 @@ class SphericalCoordinates(Coordinates):
 
     def to_cartesian(self) -> CartesianCoordinates:
 
-        coords_cartesian = _spherical_to_cartesian(self.coords.filled())
+        coords_cartesian = spherical_to_cartesian(self.coords.filled())
         coords_cartesian = np.array(coords_cartesian)
 
         if self.covariances is not None:
-            warnings.warn("Covariance transformations have not been implemented yet.")
+            covariances_cartesian = transform_covariances_jacobian(
+                self.coords.filled(),
+                self.covariances.filled(),
+                _spherical_to_cartesian
+            )
+        else:
+            covariances_cartesian = None
 
         coords = CartesianCoordinates(
             x=coords_cartesian[:, 0],
@@ -257,7 +331,7 @@ class SphericalCoordinates(Coordinates):
             vy=coords_cartesian[:, 4],
             vz=coords_cartesian[:, 5],
             times=self.times,
-            covariances=None,
+            covariances=covariances_cartesian,
             origin=self.origin,
             frame=self.frame
         )
@@ -266,11 +340,17 @@ class SphericalCoordinates(Coordinates):
     @classmethod
     def from_cartesian(cls, cartesian: CartesianCoordinates):
 
-        coords_spherical = _cartesian_to_spherical(cartesian.coords.filled())
+        coords_spherical = cartesian_to_spherical(cartesian.coords.filled())
         coords_spherical = np.array(coords_spherical)
 
         if cartesian.covariances is not None:
-            warnings.warn("Covariance transformations have not been implemented yet.")
+            covariances_spherical = transform_covariances_jacobian(
+                cartesian.coords.filled(),
+                cartesian.covariances.filled(),
+                _cartesian_to_spherical
+            )
+        else:
+            covariances_spherical = None
 
         coords = cls(
             rho=coords_spherical[:, 0],
@@ -280,7 +360,7 @@ class SphericalCoordinates(Coordinates):
             vlon=coords_spherical[:, 4],
             vlat=coords_spherical[:, 5],
             times=cartesian.times,
-            covariances=None,
+            covariances=covariances_spherical,
             origin=cartesian.origin,
             frame=cartesian.frame
         )
