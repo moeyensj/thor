af57164e331fa8a66f3d2bdfd1e6f3b1a7860799
Joachim Moeyens <moeyensj@gmail.com>
2022-02-01 20:30:42 -0800

    Add FittedOrbits

--- thor/orbits/orbits.py
+++ thor/orbits/orbits.py
@@ -19,7 +19,8 @@ from .classification import calc_orbit_class
 logger = logging.getLogger(__name__)
 
 __all__ = [
-    "Orbits"
+    "Orbits",
+    "FittedOrbits"
 ]
 
 
@@ -208,4 +209,88 @@ class Orbits(Indexable):
         if self._classes is not None:
             df.insert(len(df.columns), "class", self.classes)
 
+        return df
+
+class FittedOrbits(Orbits):
+
+    def __init__(self,
+            coordinates,
+            ids=None,
+            obj_ids=None,
+            members=None,
+            num_obs=None,
+            arc_length=None,
+            chi2=None,
+            rchi2=None,
+        ):
+        Orbits.__init__(
+            self,
+            coordinates=coordinates,
+            ids=ids,
+            obj_ids=obj_ids,
+        )
+
+        N = len(self)
+        if members is None:
+            self._members = [np.array([]) for i in range(N)]
+        else:
+            assert len(members) == N
+            self._members = members
+
+        if num_obs is None:
+            self._num_obs = np.zeros(N, dtype=int)
+        else:
+            assert len(num_obs) == N
+            self._num_obs = num_obs
+
+        if arc_length is None:
+            self._arc_length = np.zeros(N, dtype=float)
+        else:
+            assert len(arc_length) == N
+            self._arc_length = arc_length
+
+        if chi2 is None:
+            self._chi2 = np.array([np.NaN for i in range(N)])
+        else:
+            assert len(chi2) == N
+            self._chi2 = chi2
+
+        if rchi2 is None:
+            self._rchi2 = np.array([np.NaN for i in range(N)])
+        else:
+            assert len(rchi2) == N
+            self._rchi2 = rchi2
+
+        return
+
+    @property
+    def members(self):
+        return self._members
+
+    @property
+    def num_obs(self):
+        return self._num_obs
+
+    @property
+    def arc_length(self):
+        return self._arc_length
+
+    @property
+    def chi2(self):
+        return self._chi2
+
+    @property
+    def rchi2(self):
+        return self._rchi2
+
+    def to_df(self, time_scale="tdb", coordinate_type="cartesian"):
+
+        df = Orbits.to_df(self,
+            time_scale=time_scale,
+            coordinate_type=coordinate_type
+        )
+        df.insert(len(df.columns), "num_obs", self.num_obs)
+        df.insert(len(df.columns), "arc_length", self.arc_length)
+        df.insert(len(df.columns), "chi2", self.chi2)
+        df.insert(len(df.columns), "rchi2", self.rchi2)
         return df
