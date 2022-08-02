0a399a4cb6fd28bf617cf7c08a9e398dabcf4f30
Joachim Moeyens <moeyensj@gmail.com>
2022-08-01 21:23:55 -0700

    Use jax in calc_chi

--- thor/dynamics/chi.py
+++ thor/dynamics/chi.py
@@ -1,5 +1,13 @@
 import numpy as np
-from numba import jit
+import jax.numpy as jnp
+from jax import (
+    config,
+    jit,
+    lax
+)
+from typing import Tuple
+
+config.update("jax_enable_x64", True)
 
 from ..constants import Constants as c
 from .stumpff import calc_stumpff
@@ -10,8 +18,15 @@ __all__ = [
 
 MU = c.MU
 
-@jit(["UniTuple(f8, 7)(f8[:], f8[:], f8, f8, i8, f8)"], nopython=True, cache=True)
-def calc_chi(r, v, dt, mu=MU, max_iter=100, tol=1e-16):
+@jit
+def calc_chi(
+        r: np.ndarray,
+        v: np.ndarray,
+        dt: float,
+        mu: float = MU,
+        max_iter: int = 100,
+        tol: float = 1e-16
+    ) -> Tuple[np.float64, np.float64, np.float64, np.float64, np.float64, np.float64, np.float64]:
     """
     Calculate universal anomaly chi using Newton-Raphson.
 
@@ -45,23 +60,32 @@ def calc_chi(r, v, dt, mu=MU, max_iter=100, tol=1e-16):
     [1] Curtis, H. D. (2014). Orbital Mechanics for Engineering Students. 3rd ed.,
         Elsevier Ltd. ISBN-13: 978-0080977478
     """
-    r = np.ascontiguousarray(r)
-    v = np.ascontiguousarray(v)
-
-    v_mag = np.linalg.norm(v)
-    r_mag = np.linalg.norm(r)
-    rv_mag = np.dot(r, v) / r_mag
-    sqrt_mu = np.sqrt(mu)
+    v_mag = jnp.linalg.norm(v)
+    r_mag = jnp.linalg.norm(r)
+    rv_mag = jnp.dot(r, v) / r_mag
+    sqrt_mu = jnp.sqrt(mu)
 
     # Equations 3.48 and 3.50 in Curtis (2014) [1]
     alpha = -v_mag**2 / mu + 2 / r_mag
 
     # Equation 3.66 in Curtis (2014) [1]
-    chi = sqrt_mu * np.abs(alpha) * dt
+    chi = sqrt_mu * jnp.abs(alpha) * dt
 
-    ratio = 1e10
+    ratio = 1e15
     iterations = 0
-    while np.abs(ratio) > tol:
+    # Define parameters array (arguments that will be changing as
+    # the while loop iterates):
+    # chi, c0, c1, c2, c3, c4, c5, ratio, iterations
+    p = [chi, 0., 0., 0., 0., 0., 0., ratio, iterations]
+
+    # Define while loop body function
+    @jit
+    def _chi_newton_raphson(p):
+
+        chi = p[0]
+        ratio = p[-2]
+        iterations = p[-1]
+
         chi2 = chi**2
         psi = alpha * chi2
         c0, c1, c2, c3, c4, c5 = calc_stumpff(psi)
@@ -79,7 +103,36 @@ def calc_chi(r, v, dt, mu=MU, max_iter=100, tol=1e-16):
         ratio = f / fp
         chi -= ratio
         iterations += 1
-        if iterations >= max_iter:
-            break
+
+        p[0] = chi
+        p[1] = c0
+        p[2] = c1
+        p[3] = c2
+        p[4] = c3
+        p[5] = c4
+        p[6] = c5
+        p[7] = ratio
+        p[8] = iterations
+        return p
+
+    # Define while loop condition function
+    @jit
+    def _while_condition(p):
+        ratio = p[-2]
+        iterations = p[-1]
+        return (jnp.abs(ratio) > tol) & (iterations <= max_iter)
+
+    p = lax.while_loop(
+        _while_condition,
+        _chi_newton_raphson,
+        p
+    )
+    chi = p[0]
+    c0 = p[1]
+    c1 = p[2]
+    c2 = p[3]
+    c3 = p[4]
+    c4 = p[5]
+    c5 = p[6]
 
     return chi, c0, c1, c2, c3, c4, c5
