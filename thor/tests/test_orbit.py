b645e72d767fe4f12fe66d03b16ad833989b6d72
Joachim Moeyens <moeyensj@gmail.com>
2023-04-15 09:55:36 -0700

    Add tolerance for calcNhat tests

--- thor/tests/test_orbit.py
+++ thor/tests/test_orbit.py
@@ -15,7 +15,7 @@ def test_calcNhat_normalized():
     n_hat_norm = np.linalg.norm(n_hat, axis=1)
 
     # Each vector should have magnitude of 1.0
-    np.testing.assert_equal(np.ones(len(n_hat)), n_hat_norm)
+    np.testing.assert_allclose(np.ones(len(n_hat)), n_hat_norm, rtol=0, atol=1e-15)
     return
 
 
@@ -31,14 +31,16 @@ def test_calcNhat_orientation():
 
     # First orbit lies in the x-y plane and so should have a unit
     # vector normal equivalent to the z-axis
-    np.testing.assert_equal(np.array([0.0, 0.0, 1.0]), n_hat[0])
+    np.testing.assert_allclose(np.array([0.0, 0.0, 1.0]), n_hat[0], rtol=0, atol=1e-15)
 
     # Second orbit lies in a plane inclined 45 degrees from the x-y plane, intersecting
     # the x-y plane along the y-axis.
-    np.testing.assert_equal(np.array([-np.sqrt(2) / 2, 0.0, np.sqrt(2) / 2]), n_hat[1])
+    np.testing.assert_allclose(
+        np.array([-np.sqrt(2) / 2, 0.0, np.sqrt(2) / 2]), n_hat[1], rtol=0, atol=1e-15
+    )
 
     # Third orbit lies in a plane inclined 90 degrees from the x-y plane, intersecting
     # the x-y plane along the x-axis.
-    np.testing.assert_equal(np.array([0.0, -1.0, 0]), n_hat[2])
+    np.testing.assert_allclose(np.array([0.0, -1.0, 0]), n_hat[2], rtol=0, atol=1e-15)
 
     return
