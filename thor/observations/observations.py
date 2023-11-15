9cba465b9993f5a8cec27448941eb38b4dc8cd4f
Joachim Moeyens <moeyensj@gmail.com>
2023-11-14 16:15:17 -0800

    Add InputObservations

--- thor/observations/observations.py
+++ thor/observations/observations.py
@@ -10,6 +10,7 @@ from adam_core.observers import Observers
 from adam_core.time import Timestamp
 
 from .photometry import Photometry
+from .states import calculate_state_ids
 
 
 class ObserversWithStates(qv.Table):
@@ -17,24 +18,92 @@ class ObserversWithStates(qv.Table):
     observers = Observers.as_column()
 
 
+class InputObservations(qv.Table):
+    id = qv.StringColumn()
+    exposure_id = qv.StringColumn()
+    time = Timestamp.as_column()
+    ra = qv.Float64Column()
+    dec = qv.Float64Column()
+    ra_sigma = qv.Float64Column(nullable=True)
+    dec_sigma = qv.Float64Column(nullable=True)
+    ra_dec_cov = qv.Float64Column(nullable=True)
+    mag = qv.Float64Column()
+    mag_sigma = qv.Float64Column(nullable=True)
+    filter = qv.StringColumn()
+    observatory_code = qv.StringColumn()
+
+
 class Observations(qv.Table):
-    """
-    The Observations table stored invidual point source detections and a state ID for each unique
-    combination of detection time and observatory code. The state ID is used as reference to a specific
-    observing geometry.
-
-    The recommended constructor to use is `~Observations.from_detections_and_exposures`, as this function
-    will sort the detections by time and observatory code, and for each unique combination of the two
-    assign a unique state ID. If not using this constructor, please ensure that the detections are sorted
-    by time and observatory code and that each unique combination of time and observatory code has a unique
-    state ID.
-    """
-
     id = qv.StringColumn()
     exposure_id = qv.StringColumn()
     coordinates = SphericalCoordinates.as_column()
     photometry = Photometry.as_column()
     state_id = qv.Int64Column()
+
+    @classmethod
+    def from_input_observations(cls, observations: InputObservations) -> "Observations":
+        """
+        Create a THOR observations table from an InputObservations table. The InputObservations table
+        are sorted by ascending time and observatory code.
+
+        Parameters
+        ----------
+        observations : `~InputObservations`
+            A table of input observations.
+
+        Returns
+        -------
+        observations : `~Observations`
+            A table of THOR observations.
+        """
+        # Sort the observations by time and observatory code
+        observations_sorted = observations.sort_by(
+            ["time.days", "time.nanos", "observatory_code"]
+        )
+
+        # If the times are not in UTC, convert them to UTC
+        if observations_sorted.time.scale != "utc":
+            observations_sorted = observations_sorted.set_column(
+                "time", observations_sorted.time.rescale("utc")
+            )
+
+        # Extract the sigma and covariance values for RA and Dec
+        ra_sigma = observations_sorted.ra_sigma.to_numpy(zero_copy_only=False)
+        dec_sigma = observations_sorted.dec_sigma.to_numpy(zero_copy_only=False)
+        ra_dec_cov = observations_sorted.ra_dec_cov.to_numpy(zero_copy_only=False)
+
+        # Create the covariance matrices
+        covariance_matrices = np.full((len(observations_sorted), 6, 6), np.nan)
+        covariance_matrices[:, 1, 1] = ra_sigma**2
+        covariance_matrices[:, 2, 2] = dec_sigma**2
+        covariance_matrices[:, 1, 2] = ra_dec_cov
+        covariance_matrices[:, 2, 1] = ra_dec_cov
+        covariances = CoordinateCovariances.from_matrix(covariance_matrices)
+
+        # Create the coordinates table
+        coords = SphericalCoordinates.from_kwargs(
+            lon=observations_sorted.ra,
+            lat=observations_sorted.dec,
+            time=observations_sorted.time,
+            covariance=covariances,
+            origin=Origin.from_kwargs(code=observations_sorted.observatory_code),
+            frame="equatorial",
+        )
+
+        # Create the photometry table
+        photometry = Photometry.from_kwargs(
+            filter=observations_sorted.filter,
+            mag=observations_sorted.mag,
+            mag_sigma=observations_sorted.mag_sigma,
+        )
+
+        return cls.from_kwargs(
+            id=observations_sorted.id,
+            exposure_id=observations_sorted.exposure_id,
+            coordinates=coords,
+            photometry=photometry,
+            state_id=calculate_state_ids(coords),
+        )
 
     @classmethod
     def from_detections_and_exposures(
@@ -60,111 +129,59 @@ class Observations(qv.Table):
         # TODO: One thing we could try in the future is truncating observation times to ~1ms and using those to group
         # into indvidual states (i.e. if two observations are within 1ms of each other, they are in the same state). Again,
         # this only matters for those detections that have times that differ from the midpoint time of the exposure (LSST)
-
         # If the detection times are not in UTC, convert them to UTC
         if detections.time.scale != "utc":
             detections = detections.set_column("time", detections.time.rescale("utc"))
 
-        # Flatten the detections table (i.e. remove the nested columns). Unfortunately joins on tables
-        # with nested columns are not all supported in pyarrow
-        detections_flattened = pa.table(
-            [
-                detections.id,
-                detections.exposure_id,
-                detections.time.days,
-                detections.time.nanos,
-                detections.ra,
-                detections.ra_sigma,
-                detections.dec,
-                detections.dec_sigma,
-                detections.mag,
-                detections.mag_sigma,
-            ],
-            names=[
-                "id",
-                "exposure_id",
-                "days",
-                "nanos",
-                "ra",
-                "ra_sigma",
-                "dec",
-                "dec_sigma",
-                "mag",
-                "mag_sigma",
-            ],
+        # Join the detections and exposures tables
+        detections_flattened = detections.flattened_table()
+        exposures_flattened = exposures.flattened_table()
+        detections_exposures = detections_flattened.join(
+            exposures_flattened, ["exposure_id"], right_keys=["id"]
         )
 
-        # Extract the exposure IDs and the observatory codes from the exposures table
-        exposure_filters_obscodes = pa.table(
-            [exposures.id, exposures.filter, exposures.observatory_code],
-            names=["exposure_id", "filter", "observatory_code"],
+        # Combine chunks of joined table
+        detections_exposures = detections_exposures.combine_chunks()
+
+        # Create covariance matrices
+        sigmas = np.zeros((len(detections_exposures), 6))
+        sigmas[:, 1] = detections_exposures["ra_sigma"].to_numpy(zero_copy_only=False)
+        sigmas[:, 2] = detections_exposures["dec_sigma"].to_numpy(zero_copy_only=False)
+        covariances = CoordinateCovariances.from_sigmas(sigmas)
+
+        # Create the coordinates table
+        coordinates = SphericalCoordinates.from_kwargs(
+            lon=detections_exposures["ra"],
+            lat=detections_exposures["dec"],
+            time=Timestamp.from_kwargs(
+                days=detections_exposures["time.days"],
+                nanos=detections_exposures["time.nanos"],
+                scale="utc",
+            ),
+            covariance=covariances,
+            origin=Origin.from_kwargs(code=detections_exposures["observatory_code"]),
+            frame="equatorial",
         )
 
-        # Join the detection times and the exposure IDs so that each detection has an observatory code
-        obscode_times = detections_flattened.join(
-            exposure_filters_obscodes, ["exposure_id"]
-        )
-
-        # Group the detections by the observatory code and the detection times and then grab the unique ones
-        unique_obscode_times = obscode_times.group_by(
-            ["days", "nanos", "observatory_code"]
-        ).aggregate([])
-
-        # Now sort the unique detections by the observatory code and the detection time
-        unique_obscode_times = unique_obscode_times.sort_by(
-            [
-                ("days", "ascending"),
-                ("nanos", "ascending"),
-                ("observatory_code", "ascending"),
-            ]
-        )
-
-        # For each unique detection time and observatory code assign a unique state ID
-        unique_obscode_times = unique_obscode_times.add_column(
-            0,
-            pa.field("state_id", pa.int64()),
-            pa.array(np.arange(0, len(unique_obscode_times))),
-        )
-
-        # Join the unique observatory code and detections back to the original detections
-        detections_with_states = obscode_times.join(
-            unique_obscode_times, ["days", "nanos", "observatory_code"]
-        )
-
-        # Now sort the detections one final time by state ID
-        detections_with_states = detections_with_states.sort_by(
-            [("state_id", "ascending")]
-        )
-
-        sigmas = np.zeros((len(detections_with_states), 6))
-        sigmas[:, 1] = detections_with_states["ra_sigma"].to_numpy(zero_copy_only=False)
-        sigmas[:, 2] = detections_with_states["dec_sigma"].to_numpy(
-            zero_copy_only=False
+        # Create the photometry table
+        photometry = Photometry.from_kwargs(
+            filter=detections_exposures["filter"],
+            mag=detections_exposures["mag"],
+            mag_sigma=detections_exposures["mag_sigma"],
         )
 
         return cls.from_kwargs(
-            id=detections_with_states["id"],
-            exposure_id=detections_with_states["exposure_id"],
-            coordinates=SphericalCoordinates.from_kwargs(
-                lon=detections_with_states["ra"],
-                lat=detections_with_states["dec"],
-                time=Timestamp.from_kwargs(
-                    days=detections_with_states["days"],
-                    nanos=detections_with_states["nanos"],
-                    scale="utc",
-                ),
-                covariance=CoordinateCovariances.from_sigmas(sigmas),
-                origin=Origin.from_kwargs(
-                    code=detections_with_states["observatory_code"]
-                ),
-                frame="equatorial",
-            ),
-            photometry=Photometry.from_kwargs(
-                filter=detections_with_states["filter"],
-                mag=detections_with_states["mag"],
-                mag_sigma=detections_with_states["mag_sigma"],
-            ),
-            state_id=detections_with_states["state_id"],
+            id=detections_exposures["id"],
+            exposure_id=detections_exposures["exposure_id"],
+            coordinates=coordinates,
+            photometry=photometry,
+            state_id=calculate_state_ids(coordinates),
+        ).sort_by(
+            [
+                "coordinates.time.days",
+                "coordinates.time.nanos",
+                "coordinates.origin.code",
+            ]
         )
 
     def get_observers(self) -> ObserversWithStates:
