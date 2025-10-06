ed7399cdea2b19386e2c76efa51991885c4f0e6c
Nate Tellis <nate.tellis@gmail.com>
2025-10-06 16:54:46 -0400

    imports for iterate chunks

--- src/thor/orbits/iod.py
+++ src/thor/orbits/iod.py
@@ -14,8 +14,8 @@ from adam_core.coordinates.residuals import Residuals
 from adam_core.orbit_determination import OrbitDeterminationObservations
 from adam_core.orbits import Ephemeris, Orbits
 from adam_core.propagator import Propagator
-from adam_core.propagator.utils import _iterate_chunk_indices, _iterate_chunks
 from adam_core.ray_cluster import initialize_use_ray
+from adam_core.utils.iter import _iterate_chunk_indices, _iterate_chunks
 
 from ..clusters import ClusterMembers
 from ..observations.observations import Observations
