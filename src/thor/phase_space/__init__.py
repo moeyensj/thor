a8e9861d22c6cccfc44ac3ebbfd94ded0f6f3af8
Kathleen Kiker <kathleen@b612foundation.org>
2025-10-15 08:59:37 -0700

    updating init

--- src/thor/phase_space/__init__.py
+++ src/thor/phase_space/__init__.py
@@ -7,19 +7,25 @@ in coverage for strategic orbit placement.
 """
 
 from .coverage import (
-    generate_even_coverage_test_orbits, 
+    OrbitVolumes,
+    PhaseSpaceBounds,
+    analyze_orbit_coverage_diagnostics,
+    generate_even_coverage_test_orbits,
+    generate_custom_grid_test_orbits,
     generate_orbit_volumes_for_target_coverage,
     generate_orbits_for_coverage_with_fixed_volumes,
-    analyze_orbit_coverage_diagnostics,
-    OrbitVolumes,
-    PhaseSpaceBounds
+    create_grid_dimensions,
+    create_custom_grid_dimensions,
 )
 
 __all__ = [
     "generate_even_coverage_test_orbits",
+    "generate_custom_grid_test_orbits",
     "generate_orbit_volumes_for_target_coverage",
     "generate_orbits_for_coverage_with_fixed_volumes",
     "analyze_orbit_coverage_diagnostics",
+    "create_grid_dimensions",
+    "create_custom_grid_dimensions",
     "OrbitVolumes",
     "PhaseSpaceBounds",
 ]
