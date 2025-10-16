dfe57e453491cad848e9c795b865a6ebb8a40a9a
Kathleen Kiker <kathleen@b612foundation.org>
2025-10-15 21:05:34 -0700

    formatting

--- src/thor/orbits/attribution.py
+++ src/thor/orbits/attribution.py
@@ -11,8 +11,8 @@ import quivr as qv
 import ray
 from adam_core.coordinates.residuals import Residuals
 from adam_core.orbits import Orbits
-from adam_core.propagator import Propagator, _iterate_chunks
-from adam_core.propagator.utils import _iterate_chunk_indices
+from adam_core.propagator import Propagator
+from adam_core.propagator.utils import _iterate_chunk_indices, _iterate_chunks
 from adam_core.ray_cluster import initialize_use_ray
 from sklearn.neighbors import BallTree
 
