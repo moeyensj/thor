eef5c54ede9f9a9a1e056dfaffd1552c48e67164
Kathleen Kiker <kathleen@b612foundation.org>
2025-10-15 20:49:22 -0700

    formatting

--- src/thor/orbits/od.py
+++ src/thor/orbits/od.py
@@ -12,7 +12,8 @@ from adam_core.coordinates import CartesianCoordinates, CoordinateCovariances
 from adam_core.coordinates.residuals import Residuals
 from adam_core.orbit_determination import OrbitDeterminationObservations
 from adam_core.orbits import Orbits
-from adam_core.propagator import Propagator, _iterate_chunk_indices, _iterate_chunks
+from adam_core.propagator import Propagator
+from adam_core.propagator.utils import _iterate_chunk_indices, _iterate_chunks
 from adam_core.ray_cluster import initialize_use_ray
 from scipy.linalg import solve
 
